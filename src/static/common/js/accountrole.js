d62dae07734eb67377a8dcd4385c1123c053a598
Andy Byers <andy.byers@openlibhums.org>
2017-07-24 14:48:58 +0100

    Added enrolment to proofing. Closes #19.

--- src/static/common/js/accountrole.js
+++ src/static/common/js/accountrole.js
@@ -15,7 +15,12 @@ function add_role(user_id, role_id, journal_id){
             var user_row = $("#" + user_id ).clone();
             $("#" + user_id ).remove()
             user_row.find('td:first').html('<input name="' + enrol_type + '" value="' + user_id + ' " type="radio">')
-            user_row.find('td:last').html('0')
+            if (enrol_type == 'proofreader'){
+                user_row.find('td:last').html('Proofreader')
+            } else {
+                user_row.find('td:last').html('0')
+            }
+            
             $('#' + enrol_type + ' tbody').append(user_row);
             toastr.success('User has been granted role.')
         },
