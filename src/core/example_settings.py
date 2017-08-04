dee4192f3839e1848eb271ad79b325b17bec2eb6
Martin Eve <martin@martineve.com>
2017-08-04 17:38:35 +0100

    Change Debug to True by default

--- src/core/example_settings.py
+++ src/core/example_settings.py
@@ -37,7 +37,7 @@ sys.path.append(os.path.join(BASE_DIR, "plugins"))
 SECRET_KEY = 'uxprsdhk^gzd-r=_287byolxn)$k6tsd8_cepl^s^tms2w1qrv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
