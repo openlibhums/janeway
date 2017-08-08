d35a1479120c04d6c77d475855b9a2f2c5a5c668
Andy Byers <andy.byers@openlibhums.org>
2017-08-08 10:34:11 +0100

    Added setup env.

--- setup_env.sh
+++ setup_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get install python3 python3-pip python-pip virtualenvwrapper -y
-source /usr/local/bin/virtualenvwrapper.sh
+source /etc/bash_completion.d/virtualenvwrapper
 sudo apt-get install libxml2-dev libxslt1-dev python3-dev zlib1g-dev lib32z1-dev libffi-dev libssl-dev libjpeg-dev libmysqlclient-dev mysql-client mysql-server git -y
 mkvirtualenv janeway -p python3
 workon janeway
