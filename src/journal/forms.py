8f0c911e4f6956453006e16f626d67c716bd0ddd
Andy Byers <ajrbyers@gmail.com>
2017-08-22 16:35:25 +0100

    Contact form now can set the reply to.

--- src/journal/forms.py
+++ src/journal/forms.py
@@ -2,6 +2,7 @@ __copyright__ = "Copyright 2017 Birkbeck, University of London"
 __author__ = "Martin Paul Eve & Andy Byers"
 __license__ = "AGPL v3"
 __maintainer__ = "Birkbeck Centre for Technology and Publishing"
+
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 
@@ -21,13 +22,13 @@ class JournalForm(forms.ModelForm):
 class ContactForm(forms.ModelForm):
 
     question_template = _('What is %(num1)i %(operator)s %(num2)i? ')
-    are_you_a_robot = MathCaptchaField(label=_('Answer this question: '))
+    #are_you_a_robot = MathCaptchaField(label=_('Answer this question: '))
 
     def __init__(self, *args, **kwargs):
         subject = kwargs.pop('subject', None)
         super(ContactForm, self).__init__(*args, **kwargs)
 
-        self.fields['are_you_a_robot'].required = True
+        #self.fields['are_you_a_robot'].required = True
 
         if subject:
             self.fields['subject'].initial = subject
