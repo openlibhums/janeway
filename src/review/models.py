7eec0bbfb1484f6cc58d07e331bfd0583eb00435
Andy Byers <ajrbyers@gmail.com>
2017-09-08 12:19:32 +0100

    Added default element visibility field.

--- src/review/models.py
+++ src/review/models.py
@@ -201,6 +201,10 @@ class ReviewFormElement(models.Model):
     width = models.CharField(max_length=20, choices=element_width_choices())
     help_text = models.TextField()
 
+    default_visibility = models.BooleanField(default=True, help_text='If true, this setting will be available'
+                                                                     'to the author automatically, if false it will'
+                                                                     'be hidden to the author by default.')
+
     def __str__(self):
         return "Element: {0} ({1})".format(self.name, self.kind)
 
