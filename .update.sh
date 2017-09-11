5fc1b179bf84ebb7d8267515f091fdb3f1f6c84d
Andy Byers <ajrbyers@gmail.com>
2017-09-11 15:02:17 +0100

    Added update command.

---
+++ .update.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# This command will run an update on Janeway.
+python3 src/manage.py backup
+pip3 install -r requirements.txt
+python3 src/manage.py migrate
+python3 src/manage.py build_assets
+python3 src/manage.py collectstatic --no-input
+python3 src/manage.py sync_settings_to_journals
