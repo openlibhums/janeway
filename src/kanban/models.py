eb10d962d9d3cdd629c22b866579bfd374d2ad41
Andy Byers <andy.byers@openlibhums.org>
2017-12-01 13:03:35 +0000

    Deleted kanban app, can recover as plugin later.

--- src/kanban/models.py
+++
@@ -1,9 +0,0 @@
-__copyright__ = "Copyright 2017 Birkbeck, University of London"
-__author__ = "Martin Paul Eve & Andy Byers"
-__license__ = "AGPL v3"
-__maintainer__ = "Birkbeck Centre for Technology and Publishing"
-from __future__ import unicode_literals
-
-from django.db import models
-
-# Create your models here.
