c2279a1407be8e6cacedd128466512f84b57b40d
Andy Byers <andy.byers@openlibhums.org>
2018-02-09 12:18:36 +0000

    Added form for resending logged emails.

--- src/journal/forms.py
+++ src/journal/forms.py
@@ -6,10 +6,12 @@ __maintainer__ = "Birkbeck Centre for Technology and Publishing"
 from django import forms
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
+from django.utils.safestring import mark_safe
 
 from simplemathcaptcha.fields import MathCaptchaField
 from snowpenguin.django.recaptcha2.fields import ReCaptchaField
 from snowpenguin.django.recaptcha2.widgets import ReCaptchaWidget
+from django_summernote.widgets import SummernoteWidget
 
 from core import models as core_models
 from journal import models as journal_models
@@ -48,7 +50,6 @@ class ContactForm(forms.ModelForm):
     else:
         are_you_a_robot = forms.CharField(widget=forms.HiddenInput(), required=False)
 
-
     def __init__(self, *args, **kwargs):
         subject = kwargs.pop('subject', None)
         contacts = kwargs.pop('contacts', None)
@@ -62,8 +63,23 @@ class ContactForm(forms.ModelForm):
             for contact in contacts:
                 contact_choices.append([contact.email, '{name}, {role}'.format(name=contact.name, role=contact.role)])
             self.fields['recipient'].widget = forms.Select(choices=contact_choices)
-            #self.fields['recipient'].required = True
+            # self.fields['recipient'].required = True
 
     class Meta:
         model = core_models.Contact
         fields = ('recipient', 'sender', 'subject', 'body')
+
+
+class ResendEmailForm(forms.Form):
+    to = forms.CharField(max_length=1000, help_text='Seperate email addresses with ;')
+    subject = forms.CharField(max_length=1000)
+    body = forms.CharField(widget=SummernoteWidget)
+
+    def __init__(self, *args, **kwargs):
+        log_entry = kwargs.pop('log_entry')
+        super(ResendEmailForm, self).__init__(*args, **kwargs)
+
+        self.fields['to'].initial = '{to}; '.format(to=log_entry.to)
+        self.fields['subject'].initial = log_entry.subject
+        self.fields['body'].initial = mark_safe(log_entry.description)
+
