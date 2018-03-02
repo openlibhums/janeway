a5fa978b3346a146cb5c03c0b8bab504d216b025
Andy Byers <andy.byers@openlibhums.org>
2018-03-02 11:24:45 +0000

    Added generic html block.

---
+++ src/core/homepage_elements/html/hooks.py
@@ -0,0 +1,18 @@
+__copyright__ = "Copyright 2017 Birkbeck, University of London"
+__author__ = "Martin Paul Eve & Andy Byers"
+__license__ = "AGPL v3"
+__maintainer__ = "Birkbeck Centre for Technology and Publishing"
+
+from utils import setting_handler, models
+
+
+def yield_homepage_element_context(request, homepage_elements):
+    plugin = models.Plugin.objects.get(name='HTML')
+    try:
+        html_block_content = setting_handler.get_plugin_setting(plugin, 'html_block_content', request.journal).value
+    except IndexError:
+        html_block_content = '<p>This element has no content.</p>'
+
+    return {'html_content': html_block_content}
+
+
---
+++ src/core/homepage_elements/html/views.py
@@ -0,0 +1,31 @@
+__copyright__ = "Copyright 2017 Birkbeck, University of London"
+__author__ = "Martin Paul Eve & Andy Byers"
+__license__ = "AGPL v3"
+__maintainer__ = "Birkbeck Centre for Technology and Publishing"
+
+from django.contrib.admin.views.decorators import staff_member_required
+from django.urls import reverse
+from django.shortcuts import redirect, render
+from django.contrib import messages
+
+from utils import setting_handler, models
+
+
+@staff_member_required
+def html_settings(request):
+    plugin = models.Plugin.objects.get(name='HTML')
+    html_block_content = setting_handler.get_plugin_setting(plugin, 'html_block_content', request.journal, create=True,
+                                                            pretty='HTML Block Content').value
+    if request.POST:
+        html_block_content = request.POST.get('html_block_content')
+        setting_handler.save_plugin_setting(plugin, 'html_block_content', html_block_content, request.journal)
+        messages.add_message(request, messages.INFO, 'HTML Block updated.')
+        return redirect(reverse('home_settings_index'))
+
+    template = 'html_settings.html'
+    context = {
+        'html_block_content': html_block_content,
+        'disable_rich_text': request.GET.get('disable_rich_text', False)
+    }
+
+    return render(request, template, context)
