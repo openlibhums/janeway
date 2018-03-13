a9852a8b94e8daa2a085eb6fb481a625c2eef13e
Andy Byers <andy.byers@openlibhums.org>
2018-03-13 10:39:51 +0000

    Admin for Production. #240

--- src/production/admin.py
+++ src/production/admin.py
@@ -2,12 +2,27 @@ __copyright__ = "Copyright 2017 Birkbeck, University of London"
 __author__ = "Martin Paul Eve & Andy Byers"
 __license__ = "AGPL v3"
 __maintainer__ = "Birkbeck Centre for Technology and Publishing"
+
 from django.contrib import admin
 from production import models
 
+
+class ProductionAssignmentAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'article', 'production_manager', 'editor', 'assigned', 'closed', 'accepted_by_manager')
+    list_filter = ('article', 'production_manager', 'editor')
+    raw_id_fields = ('article', 'production_manager', 'editor', 'accepted_by_manager')
+
+
+class TypesetTaskAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'assignment', 'typesetter', 'assigned', 'notified', 'accepted', 'completed', 'editor_reviewed')
+    list_filter = ('assignment', 'typesetter', 'notified', 'editor_reviewed')
+    raw_id_fields = ('assignment', 'typesetter')
+    filter_horizontal = ('files_for_typesetting', 'galleys_loaded')
+
+
 admin_list = [
-    (models.ProductionAssignment,),
-    (models.TypesetTask,),
+    (models.ProductionAssignment, ProductionAssignmentAdmin),
+    (models.TypesetTask, TypesetTaskAdmin),
 ]
 
 [admin.site.register(*t) for t in admin_list]
