f1c7673f0b34776bad31546c162309f118d84914
Andy Byers <andy.byers@openlibhums.org>
2018-03-21 10:24:41 +0000

    Updated update.sh

--- .update.sh
+++ .update.sh
@@ -7,3 +7,4 @@ python3 src/manage.py migrate
 python3 src/manage.py build_assets
 python3 src/manage.py collectstatic --no-input
 python3 src/manage.py sync_settings_to_journals
+python3 src/manage.py install_plugins
