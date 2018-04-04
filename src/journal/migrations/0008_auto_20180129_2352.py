6700db1a5743064527e7cdc23261851a9fa55abc
Andy Byers <andy.byers@openlibhums.org>
2018-04-04 17:28:54 +0100

    Fixes migrations for 1.2.4

--- src/journal/migrations/0008_auto_20180129_2352.py
+++ src/journal/migrations/0008_auto_20180129_2352.py
@@ -14,12 +14,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='articleordering',
-            name='issue',
-            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to='journal.Issue'),
-            preserve_default=False,
-        ),
         migrations.AlterUniqueTogether(
             name='articleordering',
             unique_together=set([('article', 'issue', 'section')]),
