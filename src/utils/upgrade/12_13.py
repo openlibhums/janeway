7abe5d2cd175a249f4890223615cb58af66af4fd
Andy Byers <andy.byers@openlibhums.org>
2018-05-31 14:12:34 +0100

    Added some processing to 1.2 -> 1.3 module.

--- src/utils/upgrade/12_13.py
+++ src/utils/upgrade/12_13.py
@@ -1,4 +1,5 @@
 from journal import models as journal_models
+from submission import models as submission_models
 
 
 def run_journal_signals():
@@ -7,11 +8,27 @@ def run_journal_signals():
     so we want them to be fired on upgrade.
     :return: None
     """
-    journals = journal_models.Journal.objects.al()
+    print('Processing journal signals')
+    journals = journal_models.Journal.objects.all()
 
     for journal in journals:
+        print('Processing {journal_code}'.format(journal_code=journal.code), end='...')
         journal.save()
+        print(' [OK]')
+
+
+def process_article_workflow():
+    """
+    Processes an article and adds workflow history objects for it.
+    :return: None
+    """
+    print('Processing workflow migration')
+    for journal in journal_models.Journal.objects.all():
+        print('Working on {journal_code}'.format(journal_code=journal.code))
+        for article in submission_models.Article.objects.filter(journal=journal):
+            stage_log_objects = submission_models.ArticleStageLog.objects.filter(article=article)
 
 
 def execute():
     run_journal_signals()
+    process_article_workflow()
