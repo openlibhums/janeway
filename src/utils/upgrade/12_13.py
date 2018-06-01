8c1f79cacae3273baa3438daeaf8d3d53cfccb1e
Andy Byers <andy.byers@openlibhums.org>
2018-06-01 12:33:47 +0100

    Partical for 1.2 -> 1.3 upgrade path.

--- src/utils/upgrade/12_13.py
+++ src/utils/upgrade/12_13.py
@@ -1,5 +1,6 @@
 from journal import models as journal_models
 from submission import models as submission_models
+from core import models as core_models, workflow
 
 
 def run_journal_signals():
@@ -14,7 +15,32 @@ def run_journal_signals():
     for journal in journals:
         print('Processing {journal_code}'.format(journal_code=journal.code), end='...')
         journal.save()
+        workflow.create_default_workflow(journal)
         print(' [OK]')
+
+
+def add_workflow_log_entries(article, stage_log_objects):
+    """
+    Adds a workflow log entry for each stage log entry
+    :param article: Article object
+    :param stage_log_objects: QuerySet of ArticleStageLog objects
+    :return: None
+    """
+
+    non_workflow_stages = ['Published', 'Assigned', 'Under Revision',
+                           'Author Copyediting', 'Final Copyediting', 'Rejected']
+
+    for entry in stage_log_objects:
+        print(entry.stage_to)
+
+        if entry.stage_to == 'Under Review':
+            stage = 'Unassigned'
+        else:
+            stage = entry.stage_to
+
+        if entry.stage_to not in non_workflow_stages:
+            workflow_element = core_models.WorkflowElement.objects.get(journal=article.journal,
+                                                                       stage=stage)
 
 
 def process_article_workflow():
@@ -26,7 +52,13 @@ def process_article_workflow():
     for journal in journal_models.Journal.objects.all():
         print('Working on {journal_code}'.format(journal_code=journal.code))
         for article in submission_models.Article.objects.filter(journal=journal):
-            stage_log_objects = submission_models.ArticleStageLog.objects.filter(article=article)
+            stage_log_objects = submission_models.ArticleStageLog.objects.filter(article=article).order_by('date_time')
+
+            if article.is_import:
+                pass
+                #print('Article is import and has no workflow records.')
+            else:
+                add_workflow_log_entries(article, stage_log_objects)
 
 
 def execute():
