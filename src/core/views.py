d9bf8d618864e1b045228cf0d634337a9a2baf0e
Andy Byers <andy.byers@openlibhums.org>
2018-06-08 14:51:15 +0100

    Resetting a password marks an account as active #336

--- src/core/views.py
+++ src/core/views.py
@@ -219,6 +219,7 @@ def reset_password(request, token):
         if form.is_valid():
             password = form.cleaned_data['password_2']
             reset_token.account.set_password(password)
+            reset_token.account.is_active = True
             reset_token.account.save()
             reset_token.expired = True
             reset_token.save()
