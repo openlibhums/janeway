0b77c1e89989e08248635519ef76c02d408a6e92
Andy Byers <andy.byers@openlibhums.org>
2018-07-09 16:07:11 +0100

    Only admins and superusers can set admin and superuser status.

--- src/core/forms.py
+++ src/core/forms.py
@@ -178,6 +178,7 @@ class AdminUserForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         active = kwargs.pop('active', None)
+        request = kwargs.pop('request', None)
         super(AdminUserForm, self).__init__(*args, **kwargs)
 
         if not kwargs.get('instance', None):
@@ -186,6 +187,14 @@ class AdminUserForm(forms.ModelForm):
         if active == 'add':
             self.fields['password_1'] = forms.CharField(widget=forms.PasswordInput, label="Password")
             self.fields['password_2'] = forms.CharField(widget=forms.PasswordInput, label="Repeat password")
+
+        if request and not request.user.is_admin:
+            print(1)
+            self.fields.pop('is_staff', None)
+            self.fields.pop('is_admin', None)
+
+        if request and not request.user.is_superuser:
+            self.fields.pop('is_superuser')
 
     def clean_password_2(self):
         password_1 = self.cleaned_data.get("password_1")
