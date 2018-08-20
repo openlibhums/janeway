c80dba661a50bf3220fa63b5e2eaad2c6678821d
Andy Byers <andy.byers@openlibhums.org>
2018-08-20 12:14:59 +0100

    BAH! See previous commit.

--- src/utils/upgrade/shared.py
+++ src/utils/upgrade/shared.py
@@ -11,6 +11,8 @@ def versions():
 
     versions = json.loads(file.read())
 
+    return versions
+
 
 def current_version():
     versions = models.Version.objects.all().order_by('-date')
@@ -19,4 +21,12 @@ def current_version():
         return versions[0]
 
     else:
+        pass
         # Its possible that versioning didn't exist as it was only introduced in 1.3.
+
+
+def set_version(version): 
+    obj, created = models.Version.objects.get_or_create(number=version) 
+    
+    if not created: 
+        print('WARNING: This version was already found so was not created.') 
