e86470bd225f4c2b2ab92dc116719bf899b18190
Andy Byers <andy.byers@openlibhums.org>
2018-08-20 12:31:00 +0100

    Added check_versions.

--- src/utils/upgrade/shared.py
+++ src/utils/upgrade/shared.py
@@ -25,8 +25,16 @@ def current_version():
         # Its possible that versioning didn't exist as it was only introduced in 1.3.
 
 
-def set_version(version): 
-    obj, created = models.Version.objects.get_or_create(number=version) 
-    
-    if not created: 
-        print('WARNING: This version was already found so was not created.') 
+def set_version(version):
+    obj, created = models.Version.objects.get_or_create(number=version)
+
+    if not created:
+        print('WARNING: This version was already found so was not created.')
+
+
+def check_version(script):
+    version = current_version()
+
+    if version and version.number == script:
+        input('This upgrade appears to have already been run. '
+              'Press [Enter] to continue running, press [Ctrl + C] to exit.')
