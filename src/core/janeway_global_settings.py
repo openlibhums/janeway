440c10750411a157aed3d43ddae2b0bfa60f02f9
Mauro MSL <mauro.sanchez@openlibhums.org>
2018-12-19 20:17:12 +0000

    #558: Removes sites from global settings

--- src/core/janeway_global_settings.py
+++ src/core/janeway_global_settings.py
@@ -55,7 +55,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
     'django.contrib.humanize',
 
     # Installed Apps
