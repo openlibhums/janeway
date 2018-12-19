40c6df3dc3eddfe67f3e5bca2a72469b5f20d538
Mauro MSL <mauro.sanchez@openlibhums.org>
2018-12-19 20:16:31 +0000

    #558: Migration wasn't sqlite compatible

--- src/core/migrations/0019_adds_DomainAlias_journal_FK.py
+++ src/core/migrations/0019_adds_DomainAlias_journal_FK.py
@@ -1,28 +1,37 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import migrations, models
+from django.db import migrations, models, connection, utils
 import django.db.models.deletion
 
 
 JOURNAL_SITESECTOMY = """
-    UPDATE core_domainalias da SET journal_id = (
+    UPDATE core_domainalias SET journal_id = (
         SELECT j.id
-            FROM journal_journal j, django_site s
-            WHERE s.id = da.site_id
+            FROM journal_journal as j, django_site as s
+            WHERE s.id = core_domainalias.site_id
             AND s.domain = j.domain
         );
 """
 
 PRESS_SITESECTOMY = """
-    UPDATE core_domainalias da SET press_id = (
+    UPDATE core_domainalias SET press_id = (
         SELECT p.id
-            FROM press_press p, django_site s
-            WHERE s.id = da.site_id
+            FROM press_press as p, django_site as
+            WHERE s.id = core_domainalias.site_id
             AND s.domain = p.domain
         );
 """
 
+
+def sitesectomy(*args, **kwargs):
+    with connection.cursor() as cursor:
+        try:
+            cursor.execute(JOURNAL_SITESECTOMY)
+            cursor.execute(PRESS_SITESECTOMY)
+        except utils.OperationalError:
+            # New installations won't have a django_sites table
+            pass
 
 class Migration(migrations.Migration):
 
@@ -52,6 +61,7 @@ class Migration(migrations.Migration):
                 null=True,
                 on_delete=django.db.models.deletion.CASCADE, to='journal.Journal'),
         ),
-        migrations.RunSQL(JOURNAL_SITESECTOMY, reverse_sql=migrations.RunSQL.noop),
-        migrations.RunSQL(PRESS_SITESECTOMY, reverse_sql=migrations.RunSQL.noop),
+        migrations.RunPython(sitesectomy, reverse_code=migrations.RunPython.noop),
+#        migrations.RunSQL(JOURNAL_SITESECTOMY, reverse_sql=migrations.RunSQL.noop),
+#        migrations.RunSQL(PRESS_SITESECTOMY, reverse_sql=migrations.RunSQL.noop),
     ]
