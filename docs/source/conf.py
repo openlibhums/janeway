263a345e07bb6d5a3a80bbc80218fa9f74585ffa
Andy Byers <andy.byers@openlibhums.org>
2018-12-22 13:58:11 +0000

    Added some custom CSS. Working on submission settings.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -87,7 +87,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['nstatic']
+html_static_path = ['nstatic', '_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -176,3 +176,6 @@ epub_exclude_files = ['search.html']
 
 
 # -- Extension configuration -------------------------------------------------
+
+def setup(app):
+    app.add_stylesheet('janeway.css')
