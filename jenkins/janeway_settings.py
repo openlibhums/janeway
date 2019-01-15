2920343e8e10ec29f299ba3c3b7ab50c190b0ff6
Mauro MSL <mauro.sanchez@openlibhums.org>
2019-01-15 10:49:10 +0000

    Completes previous commit

--- jenkins/janeway_settings.py
+++ jenkins/janeway_settings.py
@@ -4,6 +4,7 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
 NOSE_ARGS = [
     '--with-xunit',
+    '--xunit-file=jenkins/nosetests.xml',
     '--cover-erase',
     '--cover-package=janeway', # Change `MY_APP` to your `app` name
 ]
