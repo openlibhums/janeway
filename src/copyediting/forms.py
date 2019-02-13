129b7d466f60637cc2dcea78241f0d90cd49f761
Andy Byers <andy.byers@openlibhums.org>
2019-02-13 17:21:40 +0000

    Moved logic into form rather than view.

--- src/copyediting/forms.py
+++ src/copyediting/forms.py
@@ -6,13 +6,55 @@ __maintainer__ = "Birkbeck Centre for Technology and Publishing"
 
 from django import forms
 
+from core.models import Account
 from copyediting import models
 
 
 class CopyeditAssignmentForm(forms.ModelForm):
     class Meta:
         model = models.CopyeditAssignment
-        fields = ('editor_note', 'due')
+        fields = (
+            'editor_note',
+            'due',
+            'files_for_copyediting',
+            'copyeditor'
+        )
+
+        attrs = {'required': True}
+
+        widgets = {
+            'files_for_copyediting': forms.CheckboxSelectMultiple(attrs=attrs),
+            'copyeditor': forms.RadioSelect(attrs=attrs),
+        }
+
+    def __init__(self, *args, **kwargs):
+        copyeditor_pks = kwargs.pop('copyeditor_pks')
+        files = kwargs.pop('files')
+        super(CopyeditAssignmentForm, self).__init__(*args, **kwargs)
+        self.fields['copyeditor'].queryset = Account.objects.filter(
+            pk__in=copyeditor_pks
+        )
+        self.fields['files_for_copyediting'].queryset = files
+
+    def save(self, editor=None, article=None, commit=True):
+        copyedit = super(CopyeditAssignmentForm, self).save(commit=False)
+        copyedit.copyeditor = self.cleaned_data.get('copyeditor')
+
+        if editor:
+            copyedit.editor = editor
+
+        if article:
+            copyedit.article = article
+
+        if commit:
+            copyedit.save()
+
+            # If saving, an instance exists so we can now add the files.
+            copyedit.files_for_copyediting.add(
+                self.cleaned_data.get('files_for_copyediting')
+            )
+
+        return copyedit
 
 
 class CopyEditForm(forms.ModelForm):
