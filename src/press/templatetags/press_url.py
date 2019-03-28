6ff86649f4208322bf5ee466c19d70d9cbd7cbc1
Andy Byers <andy.byers@openlibhums.org>
2019-03-28 21:53:24 +0000

    Updates OLH base templates.

--- src/press/templatetags/press_url.py
+++ src/press/templatetags/press_url.py
@@ -1,9 +1,10 @@
 import mimetypes
+import os
 
 from django import template
 from django.utils.safestring import mark_safe
 from django.urls import reverse
-from django.http import Http404
+from django.conf import settings
 
 from press import models as press_models
 register = template.Library()
@@ -15,18 +16,23 @@ def press_url(request):
 
 
 @register.simple_tag
-def svg(filename):
+def svg(filename, partial=False):
     path = filename
 
     if not path:
         return None
 
     mimetype = mimetypes.guess_type(path, strict=True)
+
     if not mimetype or mimetype[0] != 'image/svg+xml':
+        print(1)
         return mark_safe('<img src="{url}" class="top-bar-image">'.format(url=reverse('press_cover_download')))
 
     if isinstance(path, (list, tuple)):
         path = path[0]
+
+    if partial:
+        path = os.path.join(settings.BASE_DIR, path)
 
     try:
         with open(path) as svg_file:
