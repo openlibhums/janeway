40a9e71942ba3d785895f668ecd243c5c3f656b7
Mauro MSL <mauro.sanchez@openlibhums.org>
2019-04-08 11:15:55 +0100

    Updates upgrade script for 1.3.4

--- .update.sh
+++ .update.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 # This command will run an update on Janeway.
-python3 src/manage.py backup
+
 pip3 install -r requirements.txt
+python3 src/manage.py backup
 python3 src/manage.py migrate
 python3 src/manage.py build_assets
 python3 src/manage.py collectstatic --no-input
-python3 src/manage.py sync_settings_to_journals
+python3 src/manage.py load_default_settings
 python3 src/manage.py install_plugins
