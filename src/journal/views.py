8bdb1c4554f686a48b45d0ee22f0de7d1d8dc6f9
Mauro MSL <mauro.msl@outlook.com>
2019-05-22 11:59:06 +0100

    #1136: Updates all core themes

--- src/journal/views.py
+++ src/journal/views.py
@@ -226,7 +226,7 @@ def issue(request, issue_id, show_sidebar=True):
     context = {
         'issue': issue_object,
         'issues': issue_objects,
-        'structure': issue_object.structure,
+        'structure': issue_object.structure, # for backwards compatibility
         'articles': articles,
         'editors': editors,
         'show_sidebar': show_sidebar,
