7419a93def439264d6cd69bd3f46143f3859a582
Andy Byers <andy.byers@openlibhums.org>
2019-06-14 14:50:35 +0100

    #959 Makes title of about block configurable.

--- src/core/homepage_elements/about/views.py
+++ src/core/homepage_elements/about/views.py
@@ -7,19 +7,42 @@ from django.urls import reverse
 from django.shortcuts import redirect, render
 from django.contrib import messages
 
+from core.homepage_elements.about import forms, plugin_settings
 from security.decorators import editor_user_required
+from utils.setting_handler import get_plugin_setting
 
 
 @editor_user_required
 def journal_description(request):
+
+    title = get_plugin_setting(
+        plugin_settings.get_self(),
+        'about_title',
+        request.journal,
+        create=True,
+    )
+
+    description = request.journal.get_setting('general', 'journal_description')
+
+    form = forms.AboutForm(
+        initial={
+            'title': title.value if title.value else 'About {name}'.format(
+                name=request.journal.name,
+            ),
+            'description': description,
+        }
+    )
+
     if request.POST:
-        description = request.POST.get('description')
-        request.journal.description = description
-        request.journal.save()
-        messages.add_message(request, messages.INFO, 'Description updated.')
-        return redirect(reverse('journal_description'))
+        form = forms.AboutForm(request.POST)
+        if form.is_valid():
+            form.save(request.journal)
+            messages.add_message(request, messages.INFO, 'Settings updated.')
+            return redirect(reverse('journal_description'))
 
     template = 'about_settings.html'
-    context = {}
+    context = {
+        'form': form,
+    }
 
     return render(request, template, context)
