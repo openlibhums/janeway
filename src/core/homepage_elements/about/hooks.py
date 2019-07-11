7a821abeee9ed1450a5953ac2cbb65ba9c1454d9
Andy Byers <a.byers@bbk.ac.uk>
2019-07-11 15:09:38 +0100

    Added ugettext to About this Journal.

--- src/core/homepage_elements/about/hooks.py
+++ src/core/homepage_elements/about/hooks.py
@@ -3,6 +3,8 @@ __author__ = "Martin Paul Eve & Andy Byers"
 __license__ = "AGPL v3"
 __maintainer__ = "Birkbeck Centre for Technology and Publishing"
 
+from django.utils.translation import ugettext_lazy as _
+
 from utils.setting_handler import get_plugin_setting
 from core.homepage_elements.about import plugin_settings
 
@@ -18,7 +20,7 @@ def yield_homepage_element_context(request, homepage_elements):
             )
             title_value = title.value
         except IndexError:
-            title_value = 'About this Journal'
+            title_value = _('About this Journal')
 
         return {
             'about_content': request.journal.description,
