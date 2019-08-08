b45e1920ca90c5cbf018326e1362938902d8ccaa
Andy Byers <a.byers@bbk.ac.uk>
2019-08-08 11:54:49 +0100

    #974 adds a debug output for slack messages and fixes message for new submissions.
    Closes #974

--- src/utils/transactional_emails.py
+++ src/utils/transactional_emails.py
@@ -270,7 +270,8 @@ def send_reviewer_accepted_or_decline_acknowledgements(**kwargs):
 
 def send_submission_acknowledgement(**kwargs):
     """
-    This function is called via the event handling framework and it notifies site operators of a submission. It is
+    This function is called via the event handling framework and it
+    notifies site operators of a submission. It is
     wired up in core/urls.py.
     :param kwargs: a list of kwargs that includes article and request
     :return: None
@@ -279,31 +280,47 @@ def send_submission_acknowledgement(**kwargs):
     article = kwargs['article']
     request = kwargs['request']
 
-    util_models.LogEntry.add_entry(types='Submission Complete',
-                                   description='A new article {0} was submitted'.format(article.title),
-                                   level='Info', actor=request.user, target=article, request=request)
+    util_models.LogEntry.add_entry(
+        types='Submission Complete',
+        description='A new article {0} was submitted'.format(article.title),
+        level='Info',
+        actor=request.user,
+        target=article,
+        request=request,
+    )
 
     context = {
         'article': article,
         'request': request
     }
 
-    log_dict = {'level': 'Info',
-                'action_text': 'A new article {0} was submitted'.format(article.title),
-                'types': 'New Submission Acknowledgement',
-                'target': article}
+    log_dict = {
+        'level': 'Info',
+        'action_text': 'A new article {0} was submitted'.format(article.title),
+        'types': 'New Submission Acknowledgement',
+        'target': article,
+    }
 
     # send to slack
-    notify_helpers.send_slack(request,
-                              'New submission: {0} {1}'.format(article.title, article.get_remote_url(request)),
-                              ['slack_editors'])
+    slack_url = request.journal.site_url(
+        path=reverse('review_unassigned_article',
+                     kwargs={'article_id': article.pk})
+    )
+    notify_helpers.send_slack(
+        request,
+        'New submission: {0} {1}'.format(article.title,
+                                         slack_url),
+        ['slack_editors'])
 
     # send to author
-    notify_helpers.send_email_with_body_from_setting_template(request,
-                                                              'submission_acknowledgement',
-                                                              'subject_submission_acknowledgement',
-                                                              article.correspondence_author.email,
-                                                              context, log_dict=log_dict)
+    notify_helpers.send_email_with_body_from_setting_template(
+        request,
+        'submission_acknowledgement',
+        'subject_submission_acknowledgement',
+        article.correspondence_author.email,
+        context,
+        log_dict=log_dict,
+    )
 
     # send to all authors
     editors_to_email = setting_handler.get_setting(
