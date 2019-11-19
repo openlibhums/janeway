f05f59b8f12bd6940bc8950927e440d4fb813049
Andy Byers <andy.byers@openlibhums.org>
2019-11-19 23:09:14 +0000

    Loop over journals if no journal code is supplied.

--- src/metrics/management/commands/fetch_forward_links.py
+++ src/metrics/management/commands/fetch_forward_links.py
@@ -47,9 +47,9 @@ def process_book(link, article):
     defaults = {
         'year': link.year.contents[0],
         'title': title,
-        'component_number': link.component_number.contents[0],
-        'isbn_print': isbn_print.contents[0],
-        'isbn_electronic': isbn_elec.contents[0],
+        'component_number': link.component_number.contents[0] if link.component_number else '',
+        'isbn_print': isbn_print.contents[0] if isbn_print else '',
+        'isbn_electronic': isbn_elec.contents[0] if isbn_elec else '',
     }
     models.BookLink.objects.get_or_create(
         article=article,
@@ -73,7 +73,7 @@ class Command(BaseCommand):
         :param parser: the parser to which the required arguments will be added
         :return: None
         """
-        parser.add_argument('journal_code')
+        parser.add_argument('journal_code', nargs='?')
         parser.add_argument('date', default='2000-01-01', nargs='?')
 
     def handle(self, *args, **options):
@@ -84,65 +84,83 @@ class Command(BaseCommand):
         :return: None
         """
         date = options.get('date', '2000-01-01')
-        journal_code = options.get('journal_code')
+        journal_code = options.get('journal_code', None)
 
-        journal = jm.Journal.objects.get(code=journal_code)
-        usr = setting_handler.get_setting(
-            'Identifiers',
-            'crossref_username',
-            journal,
-        ).value
-        pwd = setting_handler.get_setting(
-            'Identifiers',
-            'crossref_password',
-            journal,
-        ).value
-        doi = setting_handler.get_setting(
-            'Identifiers',
-            'crossref_prefix',
-            journal,
-        ).value
+        if journal_code:
+            journals = jm.Journal.objects.filter(code=journal_code)
+        else:
+            journals = jm.Journal.objects.all()
 
-        payload = {
-            'usr': usr,
-            'pwd': pwd,
-            'doi': doi,
-            'startDate': date,
-        }
+        for journal in journals:
 
-        url = 'https://doi.crossref.org/servlet/getForwardLinks?{}'.format(
-            urlencode(payload)
-        )
-
-        print('Making request.', end='... ')
-        r = requests.get(url)
-        print('[ok]')
-
-        print('Souping response', end='... ')
-        soup = BeautifulSoup(r.text, 'lxml')
-        print('[ok]')
-
-        print('Finding forward links', end='... ')
-        forward_links = soup.find_all('forward_link')
-        print('[ok]')
-
-        print('Looping through links', end='... ')
-        for link in forward_links:
-            type = link.doi.get('type')
-            doi = link.get('doi')
-
-            article = sm.Article.get_article(
+            use_crossref = setting_handler.get_setting(
+                'Identifiers',
+                'use_crossref',
                 journal,
-                'doi',
-                doi,
-            )
+            ).processed_value
 
-            if article:
-                if type == 'journal_article':
-                    process_article(link, article)
-                else:
-                    process_book(link, article)
-            else:
-                print('Article with doi {} not found.'.format(doi))
+            if use_crossref:
 
-        print(len(forward_links))
+                usr = setting_handler.get_setting(
+                    'Identifiers',
+                    'crossref_username',
+                    journal,
+                ).value
+                pwd = setting_handler.get_setting(
+                    'Identifiers',
+                    'crossref_password',
+                    journal,
+                ).value
+                doi = setting_handler.get_setting(
+                    'Identifiers',
+                    'crossref_prefix',
+                    journal,
+                ).value
+
+                payload = {
+                    'usr': usr,
+                    'pwd': pwd,
+                    'doi': doi,
+                    'startDate': date,
+                }
+
+                url = 'https://doi.crossref.org/servlet/getForwardLinks?{}'.format(
+                    urlencode(payload)
+                )
+
+                print(url)
+
+                print('Making request.', end='... ')
+                r = requests.get(url)
+                print('[ok]')
+
+                print('Souping response', end='... ')
+                soup = BeautifulSoup(r.text, 'lxml')
+                print('[ok]')
+
+                print('Finding forward links', end='... ')
+                forward_links = soup.find_all('forward_link')
+                print('[ok]')
+
+                print('Looping through links', end='... ')
+                for link in forward_links:
+                    type = link.doi.get('type')
+                    doi = link.get('doi')
+
+                    article = sm.Article.get_article(
+                        journal,
+                        'doi',
+                        doi,
+                    )
+
+                    if article:
+                        if type == 'journal_article':
+                            process_article(link, article)
+                        elif type == 'book_content':
+                            process_book(link, article)
+                        else:
+                            print('Forward link type {} not supported'.format(type))
+                    else:
+                        print('Article with doi {} not found.'.format(doi))
+
+                print(len(forward_links))
