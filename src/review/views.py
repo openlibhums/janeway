66c4131c49f2627c415d598297230d047854feb4
Andy Byers <andy.byers@openlibhums.org>
2020-01-16 13:40:26 +0000

    Added url, form, view and templates.

--- src/review/views.py
+++ src/review/views.py
@@ -27,7 +27,7 @@ from security.decorators import (
     editor_is_not_author, senior_editor_user_required,
     section_editor_draft_decisions, article_stage_review_required
 )
-from submission import models as submission_models
+from submission import models as submission_models, forms as submission_forms
 from utils import models as util_models, ithenticate
 
 
@@ -135,9 +135,60 @@ def unassigned_article(request, article_id):
 
 
 @editor_user_required
+def add_projected_issue(request, article_id):
+    """
+    Allows an editor to add a projected issue to an article.
+    """
+    article = get_object_or_404(
+        submission_models.Article,
+        pk=article_id,
+    )
+
+    form = submission_forms.ProjectedIssueForm(instance=article)
+
+    if request.POST:
+        form = submission_forms.ProjectedIssueForm(
+            request.POST,
+            instance=article,
+        )
+
+        if form.is_valid():
+            form.save()
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                'Projected Issue set.'
+            )
+
+            if request.GET.get('return'):
+                return redirect(
+                    request.GET.get('return')
+                )
+            else:
+                return redirect(
+                    reverse(
+                        'review_projected_issue',
+                        kwargs={'article_id': article.pk},
+                    )
+                )
+
+    template = 'review/projected_issue.html'
+    context = {
+        'article': article,
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+
+@editor_user_required
 def view_ithenticate_report(request, article_id):
     """Allows editor to view similarity report."""
-    article = get_object_or_404(submission_models.Article, pk=article_id, ithenticate_id__isnull=False)
+    article = get_object_or_404(
+        submission_models.Article,
+        pk=article_id,
+        ithenticate_id__isnull=False,
+    )
 
     ithenticate_url = ithenticate.fetch_url(article)
 
