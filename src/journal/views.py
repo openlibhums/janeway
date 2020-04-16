335720bdd72db263c14d6e4e0d7abc3fe15c13a1
Andy Byers <andy.byers@openlibhums.org>
2020-04-16 15:17:45 +0100

    Articles view checks for stage and pub date now.

--- src/journal/views.py
+++ src/journal/views.py
@@ -132,9 +132,11 @@ def articles(request):
     pinned_articles = [pin.article for pin in models.PinnedArticle.objects.filter(
         journal=request.journal)]
     pinned_article_pks = [article.pk for article in pinned_articles]
-    article_objects = submission_models.Article.objects.filter(journal=request.journal,
-                                                               date_published__lte=timezone.now(),
-                                                               section__pk__in=filters).prefetch_related(
+    article_objects = submission_models.Article.objects.filter(
+        journal=request.journal,
+        stage=submission_models.STAGE_PUBLISHED,
+        date_published__lte=timezone.now(),
+        section__pk__in=filters).prefetch_related(
         'frozenauthor_set').order_by(sort).exclude(
         pk__in=pinned_article_pks)
 
