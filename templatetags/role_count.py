63cf08feeeaaa5df645f0edecf969dd87f6cb6d1
Andy Byers <andy.byers@openlibhums.org>
2020-05-04 11:14:20 +0100

    64 adds counts to dashboard.

---
+++ templatetags/role_count.py
@@ -0,0 +1,37 @@
+from django import template
+
+from plugins.typesetting import models, plugin_settings
+from submission import models as submission_models
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def typesetting_tasks_count(context):
+    request = context['request']
+    return models.TypesettingAssignment.objects.filter(
+        round__article__journal=request.journal,
+        completed__isnull=True,
+        cancelled__isnull=True,
+    ).count()
+
+
+@register.simple_tag(takes_context=True)
+def proofreading_tasks_count(context):
+    request = context['request']
+    return models.GalleyProofing.objects.filter(
+        round__article__journal=request.journal,
+        proofreader=request.user,
+        completed__isnull=True,
+        cancelled=False,
+    ).count()
+
+
+@register.simple_tag(takes_context=True)
+def articles_in_stage_count(context):
+    request = context['request']
+
+    return submission_models.Article.objects.filter(
+        journal=request.journal,
+        stage=plugin_settings.STAGE,
+    ).count()
