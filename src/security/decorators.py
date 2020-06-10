591fdd4e16571f24db3a49aba5b3f0a5150f54b0
Andy Byers <andy.byers@openlibhums.org>
2020-06-10 11:06:01 +0100

    Fixed is_article_preprint_editor decorator.

--- src/security/decorators.py
+++ src/security/decorators.py
@@ -998,6 +998,7 @@ def preprint_editor_or_author_required(func):
         preprint = get_object_or_404(
             preprint_models.Preprint,
             pk=kwargs['preprint_id'],
+            repository=request.repository
         )
 
         if request.user in preprint.authors or request.user.is_staff:
@@ -1020,9 +1021,16 @@ def is_article_preprint_editor(func):
 
     @base_check_required
     def wrapper(request, *args, **kwargs):
-        article = get_object_or_404(models.Article.preprints, pk=kwargs['article_id'])
+        if not base_check(request):
+            return redirect('{0}?next={1}'.format(reverse('core_login'), request.path_info))
 
-        if request.user in article.subject_editors() or request.user.is_staff:
+        preprint = get_object_or_404(
+            preprint_models.Preprint,
+            pk=kwargs['preprint_id'],
+            repository=request.repository
+        )
+
+        if request.user in preprint.subject_editors() or request.user.is_staff:
             return func(request, *args, **kwargs)
 
         deny_access(request)
