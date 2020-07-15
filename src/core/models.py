477015957c1bfd9be4b3d7fb04710d96e9353293
Andy Byers <andy.byers@openlibhums.org>
2020-07-15 15:12:09 +0100

    1680 changes based on PR feedback

--- src/core/models.py
+++ src/core/models.py
@@ -426,7 +426,6 @@ class Account(AbstractBaseUser, PermissionsMixin):
             # ArticleAuthorOrder.
             frozen_dict['order'] = frozen_author.order
             for k, v in frozen_dict.items():
-                frozen_dict['order'] = frozen_author.order
                 setattr(frozen_author, k, v)
                 frozen_author.save()
         else:
