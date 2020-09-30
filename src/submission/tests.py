7edf608e2cf96f3188d3cfcae5d95306400d7399
Mauro MSL <mauro.msl@outlook.com>
2020-09-30 16:11:44 +0100

    1831: Adds tests for snapshot logic

--- src/submission/tests.py
+++ src/submission/tests.py
@@ -8,10 +8,11 @@ from mock import Mock
 from django.http import Http404
 from django.test import TestCase
 
+from core.models import Account
 from identifiers import logic as id_logic
 from identifiers import logic as id_logic
 from journal import models as journal_models
-from submission import decorators, models
+from submission import decorators, models, logic
 from utils.install import update_xsl_files, update_settings
 
 
@@ -35,6 +36,31 @@ class SubmissionTests(TestCase):
         update_settings(journal_one, management_command=False)
 
         return journal_one
+
+    @staticmethod
+    def create_authors():
+        author_1_data = {
+            'is_active': True,
+            'password': 'this_is_a_password',
+            'salutation': 'Prof.',
+            'first_name': 'Martin',
+            'last_name': 'Eve',
+            'department': 'English & Humanities',
+            'institution': 'Birkbeck, University of London',
+        }
+        author_2_data = {
+            'is_active': True,
+            'password': 'this_is_a_password',
+            'salutation': 'Sr.',
+            'first_name': 'Mauro',
+            'last_name': 'Sanchez',
+            'department': 'English & Humanities',
+            'institution': 'Birkbeck, University of London',
+        }
+        author_1 = Account.objects.create(email="1@t.t", **author_1_data)
+        author_2 = Account.objects.create(email="2@t.t", **author_1_data)
+
+        return author_1, author_2
 
     def setUp(self):
         """
@@ -115,3 +141,99 @@ class SubmissionTests(TestCase):
             Http404, msg="Funding pages available when they shouldn't"
         ):
             decorated(request)
+
+    def test_snapshot_author_metadata_correctly(self):
+        article = models.Article.objects.create(
+            journal = self.journal_one,
+            title="Test article: a test article",
+        )
+        author, _ = self.create_authors()
+        logic.add_self_as_author(author, article)
+
+        article.snapshot_authors()
+        frozen = article.frozen_authors().all()[0]
+        keys = {'first_name', 'last_name', 'department', 'institution'}
+
+        self.assertDictEqual(
+            {k: getattr(author, k) for k in keys},
+            {k: getattr(frozen, k) for k in keys},
+        )
+
+    def test_snapshot_author_order_correctly(self):
+        article = models.Article.objects.create(
+            journal = self.journal_one,
+            title="Test article: a test article",
+        )
+        author_1, author_2 = self.create_authors()
+        logic.add_self_as_author(author_1, article)
+        logic.add_self_as_author(author_2, article)
+
+        article.snapshot_authors()
+        frozen = article.frozen_authors().all()
+        self.assertListEqual(
+            [author_1, author_2],
+            [f.author for f in article.frozen_authors().order_by("order")],
+            msg="Authors frozen in the wrong order",
+        )
+
+    def test_snapshot_author_metadata_do_not_override(self):
+        article = models.Article.objects.create(
+            journal = self.journal_one,
+            title="Test article: a test article",
+        )
+        author, _ = self.create_authors()
+        logic.add_self_as_author(author, article)
+
+        article.snapshot_authors()
+        frozen = article.frozen_authors().all()[0]
+        new_department = "New department"
+        frozen.department = new_department
+        frozen.save()
+        article.snapshot_authors(force_update=False)
+
+        self.assertEqual(
+            frozen.department, new_department,
+            msg="Frozen author edits have been overriden by snapshot_authors",
+        )
+
+    def test_snapshot_author_metadata_override(self):
+        article = models.Article.objects.create(
+            journal = self.journal_one,
+            title="Test article: a test article",
+        )
+        author, _ = self.create_authors()
+        logic.add_self_as_author(author, article)
+
+        article.snapshot_authors()
+        new_department = "New department"
+        article.frozen_authors().update(department=new_department)
+        article.snapshot_authors(force_update=True)
+        frozen = article.frozen_authors().all()[0]
+
+        self.assertEqual(
+            frozen.department, author.department,
+            msg="Frozen author edits have been overriden by snapshot_authors",
+        )
+
+    def test_snapshot_author_order_author_added_later(self):
+        article = models.Article.objects.create(
+            journal = self.journal_one,
+            title="Test article: a test article",
+        )
+        author_1, author_2 = self.create_authors()
+        logic.add_self_as_author(author_1, article)
+        logic.add_self_as_author(author_2, article)
+        no_order_author = Account.objects.create(
+            email="no_order@t.t",
+            first_name="no order",
+            last_name="no order",
+        )
+        article.authors.add(no_order_author)
+
+        article.snapshot_authors()
+        frozen = article.frozen_authors().all()
+        self.assertListEqual(
+            [author_1, author_2, no_order_author],
+            [f.author for f in article.frozen_authors().order_by("order")],
+            msg="Authors frozen in the wrong order",
+        )
