969c60623799a5117729137b8c91e75210d451ff
Mauro MSL <mauro.msl@outlook.com>
2020-10-06 09:03:34 +0100

    [OAI] Adds default verb for backwards compatibility

--- src/api/oai/views.py
+++ src/api/oai/views.py
@@ -6,9 +6,18 @@ from django.utils import timezone
 from api.oai.base import OAIPagedModelView
 from submission import models as submission_models
 
+# We default `verb` to ListRecords for backwards compatibility.
+DEFAULT_ENDPOINT = "ListRecords"
 
 def oai_view_factory(request, *args, **kwargs):
-    verb = request.GET.get("verb")
+    """ Maps an incoming OAI request to a django CB view
+    The OAI protocol uses a querystring parameter (verb) to determine
+    the resource being queried. This is not supported by Django's URL
+    router, so we do our own mapping here and pass this factory to the
+    url constructor instead of a view. As a result, all the OAI endpoints
+    will have the same django view name.
+    """
+    verb = request.GET.get("verb", DEFAULT_ENDPOINT)
     view = ROUTES.get(verb)
     return view(request, *args, **kwargs)
 
