b8b562c0707ecf080fe7f4d62e3b56c9689b958f
mauromsl <mauro.msl@outlook.com>
2021-03-05 18:53:56 +0000

    Load GeneratedForm initial data

--- src/review/forms.py
+++ src/review/forms.py
@@ -121,6 +121,12 @@ class GeneratedForm(forms.Form):
         fields_required = kwargs.pop('fields_required', True)
         answer = kwargs.pop('answer', None)
         preview = kwargs.pop('preview', None)
+        if 'initial' not in kwargs:
+            kwargs["initial"] = {
+                a.original_element.name: a.answer
+                for a in review_assignment.review_form_answers()
+                if a.original_element
+            }
         super(GeneratedForm, self).__init__(*args, **kwargs)
 
         if answer:
