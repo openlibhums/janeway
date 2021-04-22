dd78aa4b1209f1346d1173a61c57aae46350758a
mauromsl <mauro.msl@outlook.com>
2021-04-22 16:07:14 +0100

    Add compilemessages command to update script

--- .update.sh
+++ .update.sh
@@ -7,5 +7,6 @@ python3 src/manage.py backup
 python3 src/manage.py migrate
 python3 src/manage.py build_assets
 python3 src/manage.py collectstatic --no-input
+DJANGO_DEFAULT_SETTINGS=1 python3 src/manage.py compilemessages
 python3 src/manage.py load_default_settings
 python3 src/manage.py install_plugins
