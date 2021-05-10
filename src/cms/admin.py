0d9eb46ec257ad129e489e06e67262d173437ddb
Andy Byers <a.byers@bbk.ac.uk>
2021-05-10 14:31:41 +0100

    Closes #2170. Closes #2160

--- src/cms/admin.py
+++ src/cms/admin.py
@@ -6,9 +6,14 @@ __maintainer__ = "Birkbeck Centre for Technology and Publishing"
 from django.contrib import admin
 from cms import models
 
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('display_name', 'name', 'object')
+
+
 admin_list = [
     (models.NavigationItem,),
-    (models.Page,),
+    (models.Page, PageAdmin),
 ]
 
 [admin.site.register(*t) for t in admin_list]
