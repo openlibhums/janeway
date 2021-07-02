0bc60cfdca4d009549b81424dd388eedc1b86f8c
Andy Byers <a.byers@bbk.ac.uk>
2021-07-02 11:55:53 +0100

    Support : in default settings URLs.

--- src/core/include_urls.py
+++ src/core/include_urls.py
@@ -82,7 +82,7 @@ urlpatterns = [
     url(r'^manager/settings/group/(?P<setting_group>[-\w.:]+)/setting/(?P<setting_name>[-\w.]+)/$',
         core_views.edit_setting,
         name='core_edit_setting'),
-    url(r'^manager/settings/group/(?P<setting_group>[-\w.]+)/default_setting/(?P<setting_name>[-\w.]+)/$',
+    url(r'^manager/settings/group/(?P<setting_group>[-\w.:]+)/default_setting/(?P<setting_name>[-\w.]+)/$',
         core_views.edit_setting,
         name='core_edit_default_setting'),
     url(r'^manager/settings/(?P<group>[-\w.]+)/$', core_views.edit_settings_group, name='core_edit_settings_group'),
