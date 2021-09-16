8d307c2dc5da3bc8ae8d72b5846684efa2598eb8
mauromsl <mauro.msl@outlook.com>
2021-09-16 16:10:34 +0100

    Fix smooth scrolling breaking new navigation

--- src/themes/material/assets/material.js
+++ src/themes/material/assets/material.js
@@ -27,12 +27,14 @@ var $root = $('html, body');
 
 $('a[href^="#"]').click(function() {
     var href = $.attr(this, 'href');
+    if (href && href != "#!") {
 
-    $root.animate({
-        scrollTop: $(href).offset().top
-    }, 500, function () {
-        window.location.hash = href;
-    });
+        $root.animate({
+            scrollTop: $(href).offset().top
+        }, 500, function () {
+            window.location.hash = href;
+        });
+    }
 
     return false;
 });
