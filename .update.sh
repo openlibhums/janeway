f768d2b4a9cc1b6db0b1d2ba3586f086e647bd2b
Andy Byers <ajrbyers@gmail.com>
2021-11-08 15:36:30 +0000

    Updated the compilemessages command per @mauromsl suggestion

--- .update.sh
+++ .update.sh
@@ -6,7 +6,7 @@ pip3 install -r requirements.txt
 python3 src/manage.py migrate
 python3 src/manage.py build_assets
 python3 src/manage.py collectstatic --no-input
-DJANGO_DEFAULT_SETTINGS=1 python3 src/manage.py compilemessages
+DJANGO_SETTINGS_MODULE=1 python3 src/manage.py compilemessages
 python3 src/manage.py load_default_settings
 python3 src/manage.py install_plugins
 python3 src/manage.py update_translation_fields
