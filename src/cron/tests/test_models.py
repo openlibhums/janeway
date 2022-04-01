ba66a81b12da2a39fbc17ec15bcf710c723a0498
Andy Byers <a.byers@bbk.ac.uk>
2022-04-01 12:32:19 +0100

    #2818 updated query, adjusted tests.

--- src/cron/tests/test_models.py
+++ src/cron/tests/test_models.py
@@ -17,6 +17,7 @@ class ModelTests(TestCase):
             journal=cls.journal_one,
             reminder_type='review',
         )
+        cls.review_assignment = helpers.create_review_assignment(journal=cls.journal_two)
 
     def test_items_for_reminder(self):
-        self.assertEqual(1,len(self.review_reminder.items_for_reminder()))
+        self.assertEqual(1, len(self.review_reminder.items_for_reminder()))
