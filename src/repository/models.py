89e3570a11457eecb8405e792963e6ce117ea901
Andy Byers <a.byers@bbk.ac.uk>
2022-04-20 13:12:16 +0100

    WIP

--- src/repository/models.py
+++ src/repository/models.py
@@ -1087,7 +1087,7 @@ def review_status_choices():
     )
 
 
-class InvitedComment(models.Model):
+class Review(models.Model):
     preprint = models.ForeignKey(
         'Preprint',
         on_delete=models.CASCADE,
