965f6b78d6a5aaeb5e201e8d9f813224e54cb6c6
Andy Byers <a.byers@bbk.ac.uk>
2022-05-09 11:43:45 +0100

    #2834 reloading of Repo settings now called by update.sh

--- .update.sh
+++ .update.sh
@@ -8,6 +8,7 @@ python3 src/manage.py build_assets
 python3 src/manage.py collectstatic --no-input
 DJANGO_SETTINGS_MODULE=1 python3 src/manage.py compilemessages
 python3 src/manage.py load_default_settings
+python3 src/manage.py update_repository_settings
 python3 src/manage.py install_plugins
 python3 src/manage.py update_translation_fields
 echo "REMINDER: don't forget to restart your webserver!"
