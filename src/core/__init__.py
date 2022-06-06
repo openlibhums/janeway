d695a05926b04c0ddf540cc217662ea9c64f3f98
mauromsl <mauro.msl@outlook.com>
2022-06-06 20:10:15 +0200

    #2876: Update indexes for searching with mysql

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -2,3 +2,5 @@ __copyright__ = "Copyright 2017 Birkbeck, University of London"
 __author__ = "Martin Paul Eve & Andy Byers"
 __license__ = "AGPL v3"
 __maintainer__ = "Birkbeck Centre for Technology and Publishing"
+
+default_app_config = 'core.apps.CoreConfig'
