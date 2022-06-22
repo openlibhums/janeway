6d36f633aa1b9451baf2790a0a15376f21c441e7
Andy Byers <a.byers@bbk.ac.uk>
2022-06-22 13:33:04 +0100

    #2936 popup email logic and forms dropped from journal app.

--- src/journal/forms.py
+++ src/journal/forms.py
@@ -7,7 +7,6 @@ from django import forms
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 from django.utils.safestring import mark_safe
-from django.core.validators import validate_email, ValidationError
 
 from simplemathcaptcha.fields import MathCaptchaField
 from snowpenguin.django.recaptcha2.fields import ReCaptchaField
@@ -73,30 +72,6 @@ class ResendEmailForm(forms.Form):
         self.fields['to'].initial = ';'.join(log_entry.to)
         self.fields['subject'].initial = log_entry.subject
         self.fields['body'].initial = mark_safe(log_entry.description)
-
-
-class EmailForm(forms.Form):
-    cc = forms.CharField(
-        required=False,
-        max_length=1000,
-        help_text='Separate email addresses with ;',
-    )
-    subject = forms.CharField(max_length=1000)
-    body = forms.CharField(widget=SummernoteWidget)
-
-    def clean_cc(self):
-        cc = self.cleaned_data['cc']
-        if not cc or cc == '':
-            return []
-
-        cc_list = [x.strip() for x in cc.split(';') if x]
-        for address in cc_list:
-            try:
-                validate_email(address)
-            except ValidationError:
-                self.add_error('cc', 'Invalid email address ({}).'.format(address))
-
-        return cc_list
 
 
 class SearchForm(forms.Form):
