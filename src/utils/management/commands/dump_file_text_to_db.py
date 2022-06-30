3aef2681b3465355fd4bcef1683edab78bbd7c16
mauromsl <mauro.msl@outlook.com>
2022-06-30 08:57:23 +0200

    #2876: Dump file text command to not break on error

--- src/utils/management/commands/dump_file_text_to_db.py
+++ src/utils/management/commands/dump_file_text_to_db.py
@@ -19,6 +19,7 @@ class Command(BaseCommand):
         parser.add_argument('--all', action="store_true", default=False)
 
     def handle(self, *args, **options):
+        errors = []
         if options["file_id"]:
             file_ = File.objects.get(id=options["file_id"])
             file_.index_full_text()
@@ -31,7 +32,15 @@ class Command(BaseCommand):
 
             for article in articles:
                 print(f"Processing Article {article.pk}")
-                article.index_full_text()
+                try:
+                    article.index_full_text()
+                except Exception as e:
+                    self.stderr.write("%s" % e)
+                    errors.append((article.id, e))
+        if errors:
+            self.stderr.write("Errors Found:")
+            for id, err in errors:
+                self.stderr.write("%d: %s" % (id, repr(err)))
 
         else:
             self.stderr.write("At least one filtering flag must be provided")
