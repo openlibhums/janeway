390cf57e399bcf01400a6054fc85c316623153d9
Joseph Muller <j.muller@bbk.ac.uk>
2022-11-03 13:05:49 +0000

    #3256 Brush up discussion admin

--- src/discussion/admin.py
+++ src/discussion/admin.py
@@ -7,16 +7,35 @@ from django.contrib import admin
 from discussion import models
 
 
+class PostInline(admin.TabularInline):
+    model = models.Post
+    extra = 0
+    raw_id_fields = ('owner', 'thread')
+    exclude = ('read_by',)
+
+
 class ThreadAdmin(admin.ModelAdmin):
     list_display = ('pk', 'subject', 'owner', 'started', 'object_title')
+    list_filter = ('subject', 'started', 'last_updated')
+    search_fields = ('pk', 'article__title', 'preprint__title',
+                     'subject', 'owner__first_name', 'owner__last_name',
+                     'owner__email', 'post__body')
     raw_id_fields = ('owner', 'article', 'preprint')
+    date_hierarchy = ('last_updated')
+
+    inlines = [
+        PostInline
+    ]
 
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('pk', 'thread', 'owner', 'posted',)
+    search_fields = ('pk', 'body', 'thread__subject', 'owner__first_name',
+                     'owner__last_name', 'owner__email',)
     raw_id_fields = ('thread', 'owner',)
     filter_horizontal = ('read_by',)
     save_as = True
+    date_hierarchy = ('posted')
 
 
 admin_list = [
