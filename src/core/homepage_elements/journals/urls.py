cb4e90e62d91dcd5dc298b7512d8c3d0b5e3379b
mauromsl <mauro.msl@outlook.com>
2022-12-09 11:35:24 +0100

    #1683: Update homepage elements for django 3.2 compatibility

--- src/core/homepage_elements/journals/urls.py
+++ src/core/homepage_elements/journals/urls.py
@@ -3,10 +3,10 @@ __author__ = "Martin Paul Eve & Andy Byers"
 __license__ = "AGPL v3"
 __maintainer__ = "Birkbeck Centre for Technology and Publishing"
 
-from django.conf.urls import url
+from django.urls import re_path
 from core.homepage_elements.journals import views
 
 urlpatterns = [
     # Featured Articles
-    url(r'^$', views.featured_journals, name='featured_journals'),
+    re_path(r'^$', views.featured_journals, name='featured_journals'),
 ]
--- src/core/homepage_elements/preprints/urls.py
+++ src/core/homepage_elements/preprints/urls.py
@@ -3,10 +3,10 @@ __author__ = "Martin Paul Eve & Andy Byers"
 __license__ = "AGPL v3"
 __maintainer__ = "Birkbeck Centre for Technology and Publishing"
 
-from django.conf.urls import url
+from django.urls import re_path
 from core.homepage_elements.preprints import views
 
 urlpatterns = [
     # Featured Articles
-    url(r'^$', views.preprints, name='preprints'),
+    re_path(r'^$', views.preprints, name='preprints'),
 ]
