584005e56069ac3bae09f3d506187ee41328a1a5
Joseph Muller <j.muller@bbk.ac.uk>
2022-12-16 11:22:35 +0000

    #3256 Improve discussion admin filters

--- src/discussion/admin.py
+++ src/discussion/admin.py
@@ -6,11 +6,13 @@ __maintainer__ = "Birkbeck Centre for Technology and Publishing"
 from django.contrib import admin
 from utils import admin_utils
 from discussion import models
+from core.templatetags.truncate import truncatesmart
 
 
 class ThreadAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'subject', 'owner', 'started', 'object_title')
-    list_filter = ('subject', 'started', 'last_updated')
+    list_display = ('pk', 'subject', 'owner', 'started', 'object_title',
+                    'journal')
+    list_filter = ('article__journal', 'subject', 'started', 'last_updated')
     search_fields = ('pk', 'article__title', 'preprint__title',
                      'subject', 'owner__first_name', 'owner__last_name',
                      'owner__email', 'post__body')
@@ -21,15 +23,25 @@ class ThreadAdmin(admin.ModelAdmin):
         admin_utils.PostInline
     ]
 
+    def journal(self, obj):
+        return obj.article.journal if obj else ''
+
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'thread', 'owner', 'posted',)
+    list_display = ('post', 'thread', 'owner', 'posted', 'journal')
+    list_filter = ('thread__article__journal', 'posted')
     search_fields = ('pk', 'body', 'thread__subject', 'owner__first_name',
                      'owner__last_name', 'owner__email',)
     raw_id_fields = ('thread', 'owner',)
     filter_horizontal = ('read_by',)
     save_as = True
     date_hierarchy = ('posted')
+
+    def journal(self, obj):
+        return obj.thread.article.journal if obj else ''
+
+    def post(self, obj):
+        return truncatesmart(obj.body) if obj else ''
 
 
 admin_list = [
