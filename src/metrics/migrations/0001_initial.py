2216abab58d0a22af77f9674a93fe5c4e16f300a
mauromsl <mauro.msl@outlook.com>
2023-02-14 19:50:23 +0100

    #3994: Migrates ArticleAccess records with wrong galley_type

--- src/metrics/migrations/0001_initial.py
+++ src/metrics/migrations/0001_initial.py
@@ -23,7 +23,7 @@ class Migration(migrations.Migration):
                 ('type', models.CharField(choices=[('download', 'Download'), ('view', 'View')], max_length=20)),
                 ('identifier', models.CharField(max_length=200)),
                 ('accessed', models.DateTimeField(default=django.utils.timezone.now)),
-                ('galley_type', models.CharField(max_length=200), null=True, blank=True),
+                ('galley_type', models.CharField(max_length=200)),
                 ('article', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='submission.Article')),
             ],
         ),
