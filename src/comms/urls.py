f7148f99cdb2c5e2ef9e9d77e9ef2f0358b40ca3
Joseph Muller <j.muller@bbk.ac.uk>
2023-02-16 22:54:23 +0000

    '#3380 New press urls for all news across journals'

--- src/comms/urls.py
+++ src/comms/urls.py
@@ -5,6 +5,8 @@ from comms import views
 urlpatterns = [
     url(r'^$', views.news_list, name='core_news_list'),
     url(r'^tag/(?P<tag>.*)/$', views.news_list, name='core_news_list_tag'),
+    url(r'^(?P<presswide>all)/$', views.news_list, name='core_news_list_presswide'),
+    url(r'^(?P<presswide>all)/tag/(?P<tag>.*)/$', views.news_list, name='core_news_list_tag_presswide'),
 
     url(r'^manager/$', views.news, name='core_manager_news'),
     url(r'^manager/edit/(?P<news_pk>\d+)/$', views.edit_news, name='core_manager_edit_news'),
