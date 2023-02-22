44def7144cbd27f87a73a973694031d4b55215ec
mauromsl <mauro.msl@outlook.com>
2023-02-22 15:59:52 +0100

    #3402: Small style change

--- src/submission/models.py
+++ src/submission/models.py
@@ -747,7 +747,9 @@ class Article(AbstractLastModifiedModel):
         year_str = ""
         if self.date_published:
             year_str = "({:%Y})".format(self.date_published)
-        journal_str = "<i>%s</i>" % self.publication_title if self.publication_title else self.journal.name
+        journal_str = "<i>%s</i>" % (
+            self.publication_title or self.journal.name
+        )
         issue_str = ""
         issue = self.issue
         if issue:
