32796498076a9712cf85f7c2286667a3b5ae0f9b
mauromsl <mauro.msl@outlook.com>
2023-03-17 13:55:07 +0100

    Ensure cleaning of OAI parameter doesn't tamper resumptionToken

--- src/api/oai/base.py
+++ src/api/oai/base.py
@@ -9,7 +9,7 @@ from datetime import datetime
 from dateutil import parser as date_parser
 from django.views.generic.list import BaseListView
 from django.views.generic.base import View, TemplateResponseMixin
-from django.utils.timezone import make_aware
+from django.utils.timezone import make_aware, utc
 
 from api.oai import exceptions
 from utils.http import allow_mutating_GET
@@ -149,6 +149,13 @@ class OAIDateFilterMixin(OAIPaginationMixin):
                 qs = qs.filter(date_published__gte=from_date)
             if self.until:
                 until_date = date_parser.parse(self.until)
+                # grab the until string and check if it is timezone aware
+                # if it is not, add a default H:m:sZ.
+                if not until_date.tzinfo:
+                    untile_date = until_date.replace(
+                            hour=23, minute=59, second=59, tzinfo=utc,
+                    )
+
                 qs = qs.filter(date_published__lte=until_date)
             return qs
         except ValueError:
@@ -183,13 +190,6 @@ class OAIDateFilterMixin(OAIPaginationMixin):
             date_str = self._decoded_token.get("until")
         else:
             date_str = self.request.GET.get("until")
-
-        if date_str:
-            # grab the until string and check if it is timezone aware
-            # if it is not, add a default H:m:sZ.
-            parsed_date = date_parser.parse(date_str)
-            if not parsed_date.tzinfo:
-                date_str = f"{date_str}T23:59:59Z"
 
         return date_str
 
