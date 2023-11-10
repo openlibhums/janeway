e858b49f01c458736b113e9e21c5b2b02b820044
Joseph Muller <j.muller@bbk.ac.uk>
2023-11-10 16:54:04 +0000

    Refactor and unit test site search indexing logic #3432

--- src/cms/logic.py
+++ src/cms/logic.py
@@ -6,6 +6,7 @@ __maintainer__ = "Open Library of Humanities"
 import os
 import glob
 import json
+from re import search
 import time
 import requests
 from urllib.parse import urlparse, urljoin
@@ -16,7 +17,7 @@ from django.core.exceptions import ImproperlyConfigured
 from journal import models as journal_models
 from repository import models as repository_models
 from cms import models as models
-from bs4 import BeautifulSoup, Tag
+from bs4 import BeautifulSoup, NavigableString, Tag
 from django.core.files.base import ContentFile
 from press import models as press_models
 from utils import setting_handler
@@ -24,58 +25,233 @@ from utils.logger import get_logger
 
 logger = get_logger(__name__)
 
-
 SITE_SEARCH_PATH = os.path.join(
     settings.MEDIA_ROOT,
     'press',
     settings.SITE_SEARCH_DIR,
 )
 
-
-def get_custom_templates_folder(journal):
-    setting = setting_handler.get_setting(
-        'general',
-        'custom_cms_templates',
-        journal,
-        default=False,
-    )
-    return setting.processed_value if setting else ''
+search_documents = []
+fetched_urls = set()
 
 
-def get_custom_templates_path(journal, press):
-    if journal:
-        theme = setting_handler.get_setting(
-            'general',
-            'journal_theme',
-            journal,
-        ).processed_value
-    elif press and press.theme:
-        theme = press.theme
+def add_search_index_document(url, name, text):
+    """
+    Adds the data for a part or page to the search index
+    """
+    data = {}
+    data['id'] = len(search_documents)
+    data['url'] = url
+    data['name'] = name
+    data['text'] = text
+    search_documents.append(data)
+
+
+def gobble_sibling_text(sibling, original_part):
+    """
+    Recursively collects a series of parallel elements for
+    indexing together, stopping before another heading
+    """
+
+    # Handle no sibling
+    if not sibling:
+        return ''
+
+    # Handle another heading
+    if sibling.name == original_part.name:
+        return ''
+    if isinstance(sibling, Tag) and sibling.find(original_part.name):
+        return ''
+
+    # Recursively get sibling text
+    if isinstance(sibling, NavigableString):
+        sibling_text = sibling
     else:
+        sibling_text = sibling.get_text()
+    next_sibling_text = gobble_sibling_text(
+        sibling.next_sibling,
+        original_part
+    )
+
+    # Decompose
+    if isinstance(sibling, NavigableString):
+        sibling.replace_with('')
+    else:
+        sibling.decompose()
+
+    return sibling_text + ' ' + next_sibling_text
+
+
+def get_text_for_parent(parent, original_part):
+    if not parent:
         return ''
 
-    folder = get_custom_templates_folder(journal)
-    if not folder:
-        return ''
+    text = gobble_sibling_text(parent.next_sibling, original_part)
+    if len(text.strip()) > len(original_part.get_text().strip()):
+        return text
+    else:
+        return get_text_for_parent(parent.parent, original_part)
 
-    return os.path.join(settings.BASE_DIR, 'themes', theme, 'templates', folder)
+
+def get_text_for_header(part):
+    """
+    Gets the wrapping section or div
+    when given an h2, h3, or h4
+    """
+
+    # Try gobbling first
+    text = gobble_sibling_text(part.next_sibling, part)
+    if len(text.strip()) > 0:
+        return text
+
+    # Otherwise go to parent
+    return get_text_for_parent(part.parent, part)
 
 
-def get_custom_templates(journal, press):
+def add_part_as_doc(part, part_url, part_name, part_text):
+    if not part_name or not part_text:
+        # HTML tree with id has no heading
+        # or no text content
+        return
+    add_search_index_document(part_url, part_name, part_text)
+    part.decompose()
 
-    templates_folder = get_custom_templates_folder(journal)
-    templates_path = get_custom_templates_path(journal, press)
-    if not templates_folder or not templates_path:
-        return []
 
-    custom_templates = [('','-----')]
-    for filepath in sorted(glob.glob(os.path.join(templates_path, '*.html'))):
-        choice = (
-            os.path.join(templates_folder, os.path.basename(filepath)),
-            os.path.basename(filepath),
-        )
-        custom_templates.append(choice)
-    return custom_templates
+def add_searchable_page_parts(url, body):
+
+    headings = ['h4', 'h3', 'h2']
+
+    for h in headings:
+        for part in body.find_all(h, id=True):
+            try:
+                part_id = part['id']
+                part_url = urljoin(url, '#' + part_id)
+                part_name = part.get_text().strip()
+                part_text = get_text_for_header(part)
+                add_part_as_doc(part, part_url, part_name, part_text)
+            except TypeError:
+                continue
+
+    for part in body.find_all(id=True):
+        try:
+            part_id = part['id']
+            part_url = urljoin(url, '#' + part_id)
+            part_name = ''
+            for h in headings:
+                if part.find(h):
+                    part_name = part.find(h).get_text().strip()
+            part_text = part.get_text()
+            add_part_as_doc(part, part_url, part_name, part_text)
+        except TypeError:
+            continue
+
+
+def get_page(url):
+    time.sleep(.1)
+    try:
+        fetched_urls.add(url)
+        headers = {
+            'Accept': 'text/html; charset=utf-8'
+        }
+        response = requests.get(url, headers=headers)
+    except requests.exceptions.ConnectionError:
+        logger.warn(f'Could not access {url}')
+        logger.warn('Please run server to index site search')
+        return
+
+    if response.status_code != 200:
+        return
+
+    if 'text/html' not in response.headers['Content-Type']:
+        return
+
+    return BeautifulSoup(response.text, 'html.parser')
+
+
+def get_name(html):
+    try:
+        return html.find('h1').get_text().strip()
+    except AttributeError:
+        return html.title.get_text().strip()
+
+
+def get_body(html):
+    body = html.find('body')
+    if not isinstance(body, Tag):
+        return
+    for non_content_selector in ['script', '.djdt-hidden']:
+        for element in body.select(non_content_selector):
+            element.decompose()
+    return body
+
+
+def get_base():
+    scheme = 'http' if settings.DEBUG else 'https'
+    press = press_models.Press.objects.first()
+    return f'{scheme}://{press.domain}'
+
+
+def excluded_urls():
+    return [
+        journal.site_url() for journal in journal_models.Journal.objects.all()
+    ] + [
+        repo.site_url() for repo in repository_models.Repository.objects.all()
+    ]
+
+
+def url_in_scope(deeper_url):
+    base = get_base()
+    if urlparse(deeper_url).hostname != urlparse(base).hostname:
+        return False
+    for excluded_url in excluded_urls():
+        if excluded_url in deeper_url:
+            return False
+    return True
+
+
+def url_is_unique(deeper_url):
+    def normalize(url):
+        fragment = urlparse(url).fragment
+        url = url.replace('#' + fragment, '')
+        if url.endswith('/'):
+            url = url[:-1]
+        return url
+
+    if deeper_url in fetched_urls:
+        return False
+    for fetched_url in fetched_urls:
+        if normalize(fetched_url) == normalize(deeper_url):
+            return False
+    return True
+
+
+def decompose_non_content_page_regions(body):
+    for non_content_selector in ['header', 'footer', 'h1']:
+        for element in body.select(non_content_selector):
+            element.decompose()
+
+
+def add_searchable_page(url):
+
+    html = get_page(url)
+    if not html:
+        return
+
+    body = get_body(html)
+    if not body:
+        return
+
+    for anchor in body.find_all('a'):
+        href = anchor.get('href', '').strip()
+        deeper_url = urljoin(url, href)
+        if url_in_scope(deeper_url) and url_is_unique(deeper_url):
+            add_searchable_page(deeper_url)
+
+    decompose_non_content_page_regions(body)
+
+    add_searchable_page_parts(url, body)
+    name = get_name(html)
+    add_search_index_document(url, name, body.get_text())
 
 
 def get_press_site_search_data():
@@ -84,104 +260,13 @@ def get_press_site_search_data():
     to be used by MiniSearch
     """
 
-    documents = []
-    indexed_urls = []
-    page_ids = {1}
-    press = press_models.Press.objects.first()
-    base = f'http://{press.domain}'
-    exclude_urls = [
-        journal.site_url() for journal in journal_models.Journal.objects.all()
-    ] + [
-        repo.site_url() for repo in repository_models.Repository.objects.all()
-    ]
+    base = get_base()
+    add_searchable_page(base)
 
-    def add_page(url):
-        time.sleep(.25)
-        try:
-            headers = {
-                'Accept': 'text/html; charset=utf-8'
-            }
-            response = requests.get(url, headers=headers)
-            indexed_urls.append(url)
-        except requests.exceptions.ConnectionError:
-            logger.warn('Please run server to index site search')
-            return
-
-        if response.status_code != 200:
-            return
-
-        if 'text/html' not in response.headers['Content-Type']:
-            return
-
-        html = BeautifulSoup(response.text, 'html.parser')
-        body = html.find('body')
-        if not isinstance(body, Tag):
-            return
-
-        try:
-            name = html.find('h1').get_text().strip()
-        except AttributeError:
-            name = html.title.get_text().strip()
-
-        for non_content_selector in ['script', '.djdt-hidden']:
-            for element in body.select(non_content_selector):
-                element.decompose()
-
-        for anchor in html.find_all('a'):
-            try:
-                path = anchor['href'].strip()
-            except KeyError:
-                continue
-            while path.startswith('//'):
-                path = path[1:]
-            path = path.split('#')[0]
-            deeper_url = urljoin(url, path)
-
-            exclude = False
-            for exclude_url in exclude_urls:
-                if exclude_url in deeper_url:
-                    exclude = True
-            for indexed_url in indexed_urls:
-                if deeper_url + '/' == indexed_url:
-                    exclude = True
-                if deeper_url == indexed_url + '/':
-                    exclude = True
-                if 'https://' + deeper_url == indexed_url:
-                    exclude = True
-                if deeper_url == 'https://' + indexed_url:
-                    exclude = True
-            if (
-                not exclude
-            ) and (
-                urlparse(deeper_url).netloc == urlparse(url).netloc
-            ) and (
-                not path.startswith('#')
-            ) and (
-                deeper_url not in indexed_urls
-            ):
-                add_page(deeper_url)
-
-        for non_content_selector in ['header', 'footer', 'h1']:
-            for element in body.select(non_content_selector):
-                element.decompose()
-
-        data = {}
-        page_id = max(page_ids) + 1
-        data['id'] = page_id
-        page_ids.add(page_id)
-        data['url'] = url
-        data['name'] = name
-        data['text'] = body.get_text()
-        documents.append(data)
-
-    path = '/'
-    url = urljoin(base, path)
-    add_page(url)
-
-    if not len(documents) > 0:
+    if not len(search_documents) > 0:
         logger.error('Search data store is empty')
 
-    return documents
+    return search_documents
 
 
 def update_search_data(press_id=1):
@@ -239,3 +324,49 @@ def get_search_data_file(press):
             'or run manage.py generate_site_search_data.'
         )
     return docs_file
+
+
+def get_custom_templates_folder(journal):
+    setting = setting_handler.get_setting(
+        'general',
+        'custom_cms_templates',
+        journal,
+        default=False,
+    )
+    return setting.processed_value if setting else ''
+
+
+def get_custom_templates_path(journal, press):
+    if journal:
+        theme = setting_handler.get_setting(
+            'general',
+            'journal_theme',
+            journal,
+        ).processed_value
+    elif press and press.theme:
+        theme = press.theme
+    else:
+        return ''
+
+    folder = get_custom_templates_folder(journal)
+    if not folder:
+        return ''
+
+    return os.path.join(settings.BASE_DIR, 'themes', theme, 'templates', folder)
+
+
+def get_custom_templates(journal, press):
+
+    templates_folder = get_custom_templates_folder(journal)
+    templates_path = get_custom_templates_path(journal, press)
+    if not templates_folder or not templates_path:
+        return []
+
+    custom_templates = [('','-----')]
+    for filepath in sorted(glob.glob(os.path.join(templates_path, '*.html'))):
+        choice = (
+            os.path.join(templates_folder, os.path.basename(filepath)),
+            os.path.basename(filepath),
+        )
+        custom_templates.append(choice)
+    return custom_templates
--- src/cms/tests/test_site_search.py
+++ src/cms/tests/test_site_search.py
@@ -11,6 +11,7 @@ from press import models as press_models
 from utils.testing import helpers
 
 from unittest.mock import patch
+from bs4 import BeautifulSoup
 
 
 class TestSiteSearch(TestCase):
@@ -20,30 +21,167 @@ class TestSiteSearch(TestCase):
         cls.press = press_models.Press.objects.first()
         if not cls.press:
             cls.press = helpers.create_press()
-        content_type = ContentType.objects.get_for_model(cls.press)
-        cls.press_contact = helpers.create_contact(content_type, cls.press.pk)
         cls.journal_one, cls.journal_two = helpers.create_journals()
         cls.docs_label = os.path.join(
             settings.SITE_SEARCH_DIR,
             f'_press_{ cls.press.pk }_documents.json'
         )
-        cls.news_item = helpers.create_news_item(content_type, cls.press.pk)
-        cls.cms_page = helpers.create_cms_page(content_type, cls.press.pk)
+        cls.searchable_page = '''
+          <!DOCTYPE html>
+          <html>
+            <head><title>Seasons</title></head>
+            <body>
+              <div>
+                <h2 id="spring">Spring</h2>
+                <p>March</p>
+                <p>April</p>
+                <div id="may">
+                  <h3>May</h3>
+                  <p>May Day</p>
+                </div>
+                <div>
+                  <div>
+                    <h2 id="summer">Summer</h2>
+                  </div>
+                  <p>June</p>
+                  <p>
+                    <a href="#thing-on-page">July</a>
+                  </p>
+                  <h2 id="autumn">Autumn</h2>
+                  <p>September</p>
+                </div>
+              </div>
+              <div id="winter">
+                <h2>Winter</h2>
+                <p>New Year</p>
+              </div>
+              <script>Do not index me</script>
+            </body>
+          </html>
+        '''
 
     @classmethod
     def tearDownClass(cls):
         cls.press.delete()
-        cls.press_contact.delete()
         cls.journal_one.delete()
         cls.journal_two.delete()
-        cls.news_item.delete()
-        cls.cms_page.delete()
 
     def tearDown(self):
+        cms_logic.search_documents = []
+        cms_logic.indexed_urls = set()
         try:
-            call_command('delete_site_search_data', '--press_id', self.press.pk)
+            call_command('delete_site_search_data')
         except FileNotFoundError:
             pass
+
+    def test_add_search_index_document(self):
+        cms_logic.add_search_index_document('example.org', 'Example', 'Hello')
+        self.assertDictEqual(
+            cms_logic.search_documents[-1],
+            {
+                'id': 0,
+                'url': 'example.org',
+                'name': 'Example',
+                'text': 'Hello',
+            }
+        )
+        self.assertEqual(len(cms_logic.search_documents), 1)
+        cms_logic.add_search_index_document('example.org/2', 'Other', 'Hi')
+        self.assertEqual(cms_logic.search_documents[-1]['id'], 1)
+        self.assertEqual(len(cms_logic.search_documents), 2)
+
+    def test_gobble_sibling_text(self):
+        soup = BeautifulSoup(self.searchable_page, 'html.parser')
+        spring = soup.find(id='spring')
+        spring_text = cms_logic.gobble_sibling_text(spring.next_sibling, spring)
+        self.assertIn('March', spring_text)
+        self.assertNotIn('June', spring_text)
+        summer = soup.find(id='summer')
+        summer_text = cms_logic.gobble_sibling_text(
+            summer.parent.next_sibling,
+            summer
+        )
+        self.assertIn('June', summer_text)
+        self.assertNotIn('August', summer_text)
+
+    def test_get_text_for_parent(self):
+        soup = BeautifulSoup(self.searchable_page, 'html.parser')
+        summer = soup.find(id='summer')
+        summer_text = cms_logic.get_text_for_parent(summer.parent, summer)
+        self.assertIn('June', summer_text)
+        self.assertNotIn('September', summer_text)
+
+    def test_get_text_for_header(self):
+        soup = BeautifulSoup(self.searchable_page, 'html.parser')
+        spring = soup.find(id='spring')
+        spring_text = cms_logic.get_text_for_header(spring)
+        self.assertIn('March', spring_text)
+        self.assertIn('May Day', spring_text)
+        summer = soup.find(id='summer')
+        summer_text = cms_logic.get_text_for_header(summer)
+        self.assertIn('June', summer_text)
+
+    @patch('cms.logic.add_search_index_document')
+    def test_add_part_as_doc(self, add_doc):
+        soup = BeautifulSoup(self.searchable_page, 'html.parser')
+        spring = soup.find(id='spring')
+        cms_logic.add_part_as_doc(spring, 'example.org', '', 'May')
+        add_doc.assert_not_called()
+        cms_logic.add_part_as_doc(spring, 'example.org', 'Spring', '')
+        add_doc.assert_not_called()
+        cms_logic.add_part_as_doc(spring, 'example.org', 'Spring', 'May')
+        add_doc.assert_called_with('example.org', 'Spring', 'May')
+        self.assertFalse(soup.find(id='spring'))
+
+    @patch('cms.logic.get_text_for_header')
+    @patch('cms.logic.add_part_as_doc')
+    def test_add_searchable_page_parts(self, add_part, get_text):
+        get_text.return_value = 'hello'
+        soup = BeautifulSoup(self.searchable_page, 'html.parser')
+        cms_logic.add_searchable_page_parts('example.org', soup.find('body'))
+        autumn = soup.find(id='autumn')
+        get_text.assert_called_with(autumn)
+        winter = soup.find(id='winter')
+        add_part.assert_called_with(
+            winter,
+            'example.org#winter',
+            'Winter',
+            '\nWinter\nNew Year\n',
+        )
+
+    def test_get_name(self):
+        html_with_h1 = BeautifulSoup('<h1>h1</h1>', 'html.parser')
+        name = cms_logic.get_name(html_with_h1)
+        self.assertEqual(name, 'h1')
+        html_with_title = BeautifulSoup('<title>title</title>', 'html.parser')
+        name = cms_logic.get_name(html_with_title)
+        self.assertEqual(name, 'title')
+
+    def test_get_body(self):
+        soup = BeautifulSoup(self.searchable_page, 'html.parser')
+        body = cms_logic.get_body(soup)
+        self.assertNotIn('Do not index me', body.get_text())
+
+    def test_excluded_urls(self):
+        self.assertListEqual(
+            [self.journal_one.site_url(), self.journal_two.site_url()],
+            cms_logic.excluded_urls()
+        )
+
+    @patch('cms.logic.get_base')
+    @patch('cms.logic.excluded_urls')
+    def test_url_in_scope(self, excluded_urls, get_base):
+        get_base.return_value = 'https://www.openlibhums.org'
+        excluded_urls.return_value = ['https://www.openlibhums.org/journal1']
+        self.assertTrue(
+            cms_logic.url_in_scope('https://www.openlibhums.org/my-site-page/')
+        )
+        self.assertFalse(
+            cms_logic.url_in_scope('https://www.openlibhums.org/journal1/a/')
+        )
+        self.assertFalse(
+            cms_logic.url_in_scope('https://www.wikipedia.org')
+        )
 
     @patch('cms.logic.update_search_data')
     def test_generate_command(self, update_search_data):
