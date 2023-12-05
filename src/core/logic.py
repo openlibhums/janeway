15c880d0896b46e86661023ef3de7d27513eeeef
Andy Byers <a.byers@bbk.ac.uk>
2023-12-05 11:39:52 +0000

    Use LANCZON instead of ANTIALIAS.

--- src/core/logic.py
+++ src/core/logic.py
@@ -134,8 +134,10 @@ def resize_and_crop(img_path, size, crop_type='middle'):
     ratio = size[0] / float(size[1])
     # The image is scaled/cropped vertically or horizontally depending on the ratio
     if ratio > img_ratio:
-        img = img.resize((size[0], int(size[0] * img.size[1] // img.size[0])),
-                         Image.ANTIALIAS)
+        img = img.resize(
+            (size[0], int(size[0] * img.size[1] // img.size[0])),
+            Image.LANCZOS,
+        )
         # Crop in the top, middle or bottom
         if crop_type == 'top':
             box = (0, 0, img.size[0], size[1])
@@ -148,7 +150,10 @@ def resize_and_crop(img_path, size, crop_type='middle'):
         img = img.crop(box)
 
     elif ratio < img_ratio:
-        img = img.resize((size[0], int(size[0] * img.size[1] // img.size[0])), Image.ANTIALIAS)
+        img = img.resize(
+            (size[0], int(size[0] * img.size[1] // img.size[0])),
+            Image.LANCZOS,
+        )
         # Crop in the top, middle or bottom
         if crop_type == 'top':
             box = (0, 0, size[0], img.size[1])
@@ -170,7 +175,7 @@ def resize_and_crop(img_path, size, crop_type='middle'):
 
         img = img.crop(box)
     else:
-        img = img.resize((size[0], size[1]), Image.ANTIALIAS)
+        img = img.resize((size[0], size[1]), Image.LANCZOS)
 
     if img.mode == "CMYK":
         img = img.convert("RGB")
