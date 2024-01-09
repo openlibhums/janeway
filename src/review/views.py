75c7b9f543a4270a6019ededc75e34f3aa66689a
Andy Byers <a.byers@bbk.ac.uk>
2024-01-09 14:49:55 +0000

    #3849 added a database agnostic way to get a list of reviews based on distinct reviewers

--- src/review/views.py
+++ src/review/views.py
@@ -2842,7 +2842,7 @@ def editor_share_reviews(request, article_id):
         journal=request.journal,
     )
     reviews = article.completed_reviews_with_decision
-    distinct_reviewers = reviews.distinct('reviewer')
+    distinct_reviewers = logic.get_distinct_reviews(reviews)
 
     for review in distinct_reviewers:
         review.email_content = logic.get_share_review_content(
