86a506349ed775aa82730708cb166d3c0faea05d
Mauro MSL <mauro.msl@outlook.com>
2024-01-24 17:50:10 +0100

    Fix conflicting migrations

--- src/repository/migrations/0042_auto_20231107_1750.py
+++ src/repository/migrations/0042_auto_20231107_1750.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('repository', '0039_alter_preprintversion_title'),
+        ('repository', '0041_auto_20231207_1658'),
     ]
 
     operations = [
