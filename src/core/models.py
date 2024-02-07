a0a5f081733b4a55ed957831acb465add0a2b0f0
Mauro MSL <mauro.msl@outlook.com>
2024-02-07 17:37:22 +0100

    #3841: Adds migration

--- src/core/models.py
+++ src/core/models.py
@@ -66,13 +66,13 @@ def profile_images_upload_path(instance, filename):
 
 
 SALUTATION_CHOICES = (
-    ('Miss', 'Miss'),
-    ('Ms', 'Ms'),
-    ('Mrs', 'Mrs'),
-    ('Mr', 'Mr'),
-    ('Mx', 'Mx'),
-    ('Dr', 'Dr'),
-    ('Prof.', 'Prof.'),
+    ('Prof.', _('Prof.')),
+    ('Miss', _('Miss')),
+    ('Ms', _('Ms')),
+    ('Mrs', _('Mrs')),
+    ('Mr', _('Mr)')),
+    ('Mx', _('Mx')),
+    ('Dr', _('Dr')),
 )
 
 COUNTRY_CHOICES = [(u'AF', u'Afghanistan'), (u'AX', u'\xc5land Islands'), (u'AL', u'Albania'),
