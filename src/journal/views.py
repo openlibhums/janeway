024bc7ef44f1c5606ecb4a42d22533ffd2a7b0bd
Mauro MSL <mauro.msl@outlook.com>
2024-02-20 10:05:25 +0100

    #3977: Download XML/PDF urls only available if article is published

--- src/journal/views.py
+++ src/journal/views.py
@@ -2435,7 +2435,7 @@ def serve_article_xml(request, identifier_type, identifier):
         identifier,
     )
 
-    if not article_object:
+    if not article_object and not article_object.is_published:
         raise Http404
 
     xml_galleys = article_object.galley_set.filter(
@@ -2503,7 +2503,7 @@ def serve_article_pdf(request, identifier_type, identifier):
         identifier,
     )
 
-    if not article_object:
+    if not article_object and not article_object.is_published:
         raise Http404
 
     pdf = article_object.pdfs.first()
