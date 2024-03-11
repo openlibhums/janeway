83f5560f8c8f6b7b133a77a7379c0ea89a9bd893
Mauro MSL <mauro.msl@outlook.com>
2024-03-11 22:51:48 +0000

    #1481: Undo bleach setting description

--- src/core/models.py
+++ src/core/models.py
@@ -700,7 +700,7 @@ class Setting(models.Model):
     )
     types = models.CharField(max_length=20, choices=setting_types)
     pretty_name = models.CharField(max_length=100, default='')
-    description = JanewayBleachField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
 
     is_translatable = models.BooleanField(default=False)
 
