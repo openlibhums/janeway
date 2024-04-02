a7249bff1ad49c7e6ab07fd24cb994797c006503
Andy Byers <a.byers@bbk.ac.uk>
2024-04-02 15:13:32 +0100

    Strip each element of the split, ignore strings that are none.

--- src/repository/views.py
+++ src/repository/views.py
@@ -356,7 +356,9 @@ def repository_search(request, search_term=None):
 
     if search_term:
         search_term = search_term.strip()
-        split_search_term = search_term.split(' ')
+        split_search_term = [
+            term.strip() for term in search_term.split(' ') if term
+        ]
 
         # Initial filter on Title, Abstract and Keywords.
         preprint_search = preprints.filter(
