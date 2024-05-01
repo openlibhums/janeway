c59fbfff0641870fef1c2ee8197517e6c7972334
Mauro MSL <mauro.msl@outlook.com>
2024-05-01 19:24:07 +0100

    JENKINS: swap runner to XMLTestRunner

--- jenkins/janeway_settings.py
+++ jenkins/janeway_settings.py
@@ -1,9 +1,3 @@
-INSTALLED_APPS = ["django_nose"]
-MERGEABLE_SETTINGS = {"INSTALLED_APPS"}
+TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.XMLTestRunner'
 
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-
-NOSE_ARGS = [
-    '--with-xunit',
-    '--xunit-file=jenkins/nosetests.xml',
-]
+TEST_OUTPUT_DIR = 'jenkins/test_results/'
