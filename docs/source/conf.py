1912800fe3651405a24acdeeff634d5018bd1c94
Mauro MSL <mauro.msl@outlook.com>
2024-05-30 17:51:57 +0100

    Bump Version 1.5.5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Andy Byers, Mauro Sanchez & Martin Paul Eve'
 author = 'Andy Byers, Mauro Sanchez & Martin Paul Eve'
 
 # The short X.Y version
-version = '1.5.4'
+version = '1.5.5'
 # The full version, including alpha/beta/rc tags
-release = '1.5.4'
+release = '1.5.5'
 
 
 # -- General configuration ---------------------------------------------------
