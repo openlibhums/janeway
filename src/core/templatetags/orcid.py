91d6f018af4c7527392cccdc2d8538be039e9d13
Esther Verreau <esther.verreau@ucop.edu>
2024-06-03 14:23:02 -0500

    add option to prefill registration form with orcid data

--- src/core/templatetags/orcid.py
+++ src/core/templatetags/orcid.py
@@ -6,10 +6,10 @@ register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def orcid_redirect_uri(context):
+def orcid_redirect_uri(context, action):
     request = context.get('request')
     if request:
-        return build_redirect_uri(request.site_type)
+        return build_redirect_uri(request.site_type, action=action)
     else:
         return ""
 
