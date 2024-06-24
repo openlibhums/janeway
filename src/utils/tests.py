04fa3fb72f2629baf4d8aa45d31db7b99b7243e0
Joseph Muller <j.muller@bbk.ac.uk>
2024-06-24 10:42:51 +0100

    Fix bug that double-encoded URL query #4291 #3899

--- src/utils/tests.py
+++ src/utils/tests.py
@@ -7,6 +7,7 @@ import io
 import os
 
 from django.apps import apps
+from django.http import QueryDict
 from django.test import TestCase, override_settings
 from django.utils import timezone, translation
 from django.core import mail
@@ -1188,6 +1189,8 @@ class TestMigrationUtils(TestCase):
                 new_value,
                 saved_value,
             )
+
+
 class TestORCiDRecord(TestCase):
 
     all_fields = {'orcid-identifier': {'uri': 'http://sandbox.orcid.org/0000-0000-0000-0000', 'path': '0000-0000-0000-0000', 'host': 'sandbox.orcid.org'}, 'preferences': {'locale': 'EN'}, 'history': {'creation-method': 'DIRECT', 'completion-date': None, 'submission-date': {'value': 1716899022299}, 'last-modified-date': {'value': 1717012729950}, 'claimed': True, 'source': None, 'deactivation-date': None, 'verified-email': True, 'verified-primary-email': True}, 'person': {'last-modified-date': {'value': 1717012710380}, 'name': {'created-date': {'value': 1716899022606}, 'last-modified-date': {'value': 1716931428927}, 'given-names': {'value': 'cdleschol'}, 'family-name': {'value': 'arship'}, 'credit-name': None, 'source': None, 'visibility': 'PUBLIC', 'path': '0000-0000-0000-0000'}, 'other-names': {'last-modified-date': None, 'other-name': [], 'path': '/0000-0000-0000-0000/other-names'}, 'biography': None, 'researcher-urls': {'last-modified-date': None, 'researcher-url': [], 'path': '/0000-0000-0000-0000/researcher-urls'}, 'emails': {'last-modified-date': {'value': 1717012710380}, 'email': [{'created-date': {'value': 1716899022599}, 'last-modified-date': {'value': 1717012710380}, 'source': {'source-orcid': {'uri': 'http://sandbox.orcid.org/0000-0000-0000-0000', 'path': '0000-0000-0000-0000', 'host': 'sandbox.orcid.org'}, 'source-client-id': None, 'source-name': {'value': 'cdleschol arship'}}, 'email': 'cdleschol@mailinator.com', 'path': None, 'visibility': 'PUBLIC', 'verified': True, 'primary': True, 'put-code': None}], 'path': '/0000-0000-0000-0000/email'}, 'addresses': {'last-modified-date': {'value': 1716931402191}, 'address': [{'created-date': {'value': 1716931402191}, 'last-modified-date': {'value': 1716931402191}, 'source': {'source-orcid': {'uri': 'http://sandbox.orcid.org/0000-0000-0000-0000', 'path': '0000-0000-0000-0000', 'host': 'sandbox.orcid.org'}, 'source-client-id': None, 'source-name': {'value': 'cdleschol arship'}}, 'country': {'value': 'US'}, 'visibility': 'PUBLIC', 'path': '/0000-0000-0000-0000/address/7884', 'put-code': 7884, 'display-index': 1}], 'path': '/0000-0000-0000-0000/address'}, 'keywords': {'last-modified-date': None, 'keyword': [], 'path': '/0000-0000-0000-0000/keywords'}, 'external-identifiers': {'last-modified-date': None, 'external-identifier': [], 'path': '/0000-0000-0000-0000/external-identifiers'}, 'path': '/0000-0000-0000-0000/person'}, 'activities-summary': {'last-modified-date': {'value': 1716931455651}, 'educations': {'last-modified-date': None, 'education-summary': [], 'path': '/0000-0000-0000-0000/educations'}, 'employments': {'last-modified-date': {'value': 1716931455651}, 'employment-summary': [{'created-date': {'value': 1716931455651}, 'last-modified-date': {'value': 1716931455651}, 'source': {'source-orcid': {'uri': 'http://sandbox.orcid.org/0000-0000-0000-0000', 'path': '0000-0000-0000-0000', 'host': 'sandbox.orcid.org'}, 'source-client-id': None, 'source-name': {'value': 'cdleschol arship'}}, 'department-name': None, 'role-title': None, 'start-date': None, 'end-date': None, 'organization': {'name': 'California Digital Library', 'address': {'city': 'Oakland', 'region': 'California', 'country': 'US'}, 'disambiguated-organization': {'disambiguated-organization-identifier': 'https://ror.org/03yrm5c26', 'disambiguation-source': 'ROR'}}, 'visibility': 'PUBLIC', 'put-code': 66225, 'path': '/0000-0000-0000-0000/employment/66225'}], 'path': '/0000-0000-0000-0000/employments'}, 'fundings': {'last-modified-date': None, 'group': [], 'path': '/0000-0000-0000-0000/fundings'}, 'peer-reviews': {'last-modified-date': None, 'group': [], 'path': '/0000-0000-0000-0000/peer-reviews'}, 'works': {'last-modified-date': None, 'group': [], 'path': '/0000-0000-0000-0000/works'}, 'path': '/0000-0000-0000-0000/activities'}, 'path': '/0000-0000-0000-0000'}
@@ -1221,3 +1224,60 @@ class TestORCiDRecord(TestCase):
         repo = helpers.create_repository(press, [], [])
         self.assertEqual(build_redirect_uri(repo), "http://localhost/login/orcid/?state=login")
         self.assertEqual(build_redirect_uri(repo, action="register"), "http://localhost/login/orcid/?state=register")
+
+
+class URLLogicTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # The unicode string of a 'next' URL
+        cls.next_url = '/target/page/?a=b&x=y'
+        # The above string url-encoded with safe='/'
+        cls.next_url_encoded = '/target/page/%3Fa%3Db%26x%3Dy'
+        # The above string prepended with 'next='
+        cls.next_url_query_string = 'next=/target/page/%3Fa%3Db%26x%3Dy'
+        # The core_login url with encoded next url
+        cls.core_login_with_next = '/login/?next=/target/page/%3Fa%3Db%26x%3Dy'
+
+    def test_build_url_query_as_querydict(self):
+        querydict = QueryDict('a=b&a=c', mutable=True)
+        querydict.update({'next': self.next_url})
+        url = logic.build_url(
+            'example.org',
+            scheme='https',
+            path='/path/',
+            query=querydict,
+        )
+        self.assertEqual(
+            url,
+            'https://example.org/path/?a=b&a=c&next=/target/page/%3Fa%3Db%26x%3Dy',
+        )
+
+    def test_build_url_query_as_plain_dict(self):
+        plain_dict = {
+            'a': 'b',
+            'next': self.next_url,
+        }
+        url = logic.build_url(
+            'example.org',
+            scheme='https',
+            path='/path/',
+            query=plain_dict,
+        )
+        self.assertEqual(
+            url,
+            'https://example.org/path/?a=b&next=/target/page/%3Fa%3Db%26x%3Dy',
+        )
+
+    def test_build_url_query_as_string(self):
+        query_string = f'a=b&a=c&{ self.next_url_query_string }'
+        url = logic.build_url(
+            'example.org',
+            scheme='https',
+            path='/path/',
+            query=query_string,
+        )
+        self.assertEqual(
+            url,
+            'https://example.org/path/?a=b&a=c&next=/target/page/%3Fa%3Db%26x%3Dy',
+        )
