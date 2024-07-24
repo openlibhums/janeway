ae7dbc40589b2d0b6eaa1861b269903c650eca67
Joseph Muller <j.muller@bbk.ac.uk>
2024-07-24 15:03:21 +0100

    Generalize and document offset_date

--- src/core/templatetags/dates.py
+++ src/core/templatetags/dates.py
@@ -5,10 +5,23 @@ from django.utils import timezone
 register = template.Library()
 
 @register.simple_tag
-def due_date(days=0, input_type="date"):
+def offset_date(days=0, input_type="date"):
     """
     Get a string representing today's date or the now datetime,
     with an offset of X days.
+    :param days: number of days from now that the date should be set
+    :param date: date or datetime-local
+
+    Usage:
+
+    <input
+      id="due"
+      name="due"
+      type="{{ input_type }}"
+      value="{% offset_date days=3 input_type=input_type %}">
+
+    See admin.core.widgets.soon_date_buttons for a use case.
+
     """
     due = timezone.now() + timezone.timedelta(days=days)
     if input_type == "date":
