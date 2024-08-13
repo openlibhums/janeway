35d8340ced6f657ded5c37ae0c5211f88f764041
Andy Byers <a.byers@bbk.ac.uk>
2024-08-13 09:22:44 +0100

    #3478 removes stray closing paragraph tag.

--- src/repository/migrations/0044_historicalrepository_review_submission_text_and_more.py
+++ src/repository/migrations/0044_historicalrepository_review_submission_text_and_more.py
@@ -14,12 +14,12 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='historicalrepository',
             name='review_submission_text',
-            field=core.model_utils.JanewayBleachField(blank=True, default='<p>Please review your submission carefully. Make any necessary changes to ensure that all information is accurate and complete.</p><p>When you are satisfied with your review click the button below to finalize your submission.</p>', help_text='Text that displays on the review page just before the author completes their submission.</p>'),
+            field=core.model_utils.JanewayBleachField(blank=True, default='<p>Please review your submission carefully. Make any necessary changes to ensure that all information is accurate and complete.</p><p>When you are satisfied with your review click the button below to finalize your submission.</p>', help_text='Text that displays on the review page just before the author completes their submission.'),
         ),
         migrations.AddField(
             model_name='repository',
             name='review_submission_text',
-            field=core.model_utils.JanewayBleachField(blank=True, default='<p>Please review your submission carefully. Make any necessary changes to ensure that all information is accurate and complete.</p><p>When you are satisfied with your review click the button below to finalize your submission.</p>', help_text='Text that displays on the review page just before the author completes their submission.</p>'),
+            field=core.model_utils.JanewayBleachField(blank=True, default='<p>Please review your submission carefully. Make any necessary changes to ensure that all information is accurate and complete.</p><p>When you are satisfied with your review click the button below to finalize your submission.</p>', help_text='Text that displays on the review page just before the author completes their submission.'),
         ),
         migrations.AlterField(
             model_name='repositoryfield',
