9051b5f8f3cac907954f0510bafd439e7d397618
Andy Byers <a.byers@bbk.ac.uk>
2024-09-24 21:53:08 +0100

    Remove import.

--- src/submission/tests.py
+++ src/submission/tests.py
@@ -908,15 +908,10 @@ class FrozenAuthorModelTest(TestCase):
         self.assertEqual('Dr. S. Bella Rogers Esq.', self.frozen_author.full_name())
 
 
-from django.test import TestCase
-from .forms import EditArticleMetadata, ArticleInfoSubmit, \
-    EditorArticleInfoSubmit
-
-
 class ArticleFormTests(TestCase):
 
     def test_competing_interests_in_edit_article_metadata(self):
-        form = EditArticleMetadata()
+        form = forms.EditArticleMetadata()
         self.assertIn(
             'competing_interests',
             form.fields,
@@ -924,7 +919,7 @@ class ArticleFormTests(TestCase):
         )
 
     def test_competing_interests_not_in_article_info_submit(self):
-        form = ArticleInfoSubmit()
+        form = forms.ArticleInfoSubmit()
         self.assertNotIn(
             'competing_interests',
             form.fields,
@@ -932,7 +927,7 @@ class ArticleFormTests(TestCase):
         )
 
     def test_competing_interests_not_in_editor_article_info_submit(self):
-        form = EditorArticleInfoSubmit()
+        form = forms.EditorArticleInfoSubmit()
         self.assertNotIn(
             'competing_interests',
             form.fields,
