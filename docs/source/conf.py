4f83d34f5fbdfd798efb61ee944c173fcab91377
Mauro MSL <mauro.msl@outlook.com>
2024-09-26 18:49:04 +0100

    Bump Version v1.6.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Andy Byers, Mauro Sanchez & Martin Paul Eve'
 # The short X.Y version
 version = '1.6.0'
 # The full version, including alpha/beta/rc tags
-release = '1.6.0-RC-2'
+release = '1.6.0'
 
 # -- General configuration ---------------------------------------------------
 
