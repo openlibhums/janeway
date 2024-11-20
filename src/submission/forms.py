e829539fa05a59ddaba0d5aaa1c1e71f75f5156c
Martin Paul Eve <martin@eve.gd>
2024-11-20 15:01:59 +0000

    feat: ability to add and change specific article JATS type

--- src/submission/forms.py
+++ src/submission/forms.py
@@ -257,7 +257,7 @@ class EditorArticleInfoSubmit(ArticleInfo):
 
 class EditArticleMetadata(ArticleInfo):
     class Meta(ArticleInfo.Meta):
-        fields = ArticleInfo.Meta.fields + ('competing_interests',)
+        fields = ArticleInfo.Meta.fields + ('competing_interests', 'jats_article_type')
 
 
 class AuthorForm(forms.ModelForm):
