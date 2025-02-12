09e473d6b433a91d5bf94801ca63484fc883dde8
Andy Byers <a.byers@bbk.ac.uk>
2025-02-12 17:09:13 +0000

    fix: update migration name to avoid conflict (#3097)

--- src/repository/migrations/0046_historicalrepository_enable_comments_and_more.py
+++ src/repository/migrations/0046_historicalrepository_enable_comments_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('repository', '0044_alter_repositoryfield_help_text'),
+        ('repository', '0045_historicalrepository_display_public_metrics_and_more'),
     ]
 
     operations = [
