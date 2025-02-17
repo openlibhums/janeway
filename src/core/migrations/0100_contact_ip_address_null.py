5885ac6c5ac5bcd9a0f41c516edf4bf6faef1a04
Joseph Muller <j.muller@bbk.ac.uk>
2025-02-17 09:57:22 +0000

    fix #4625 Fix app name in core migration dependency

--- src/core/migrations/0100_contact_ip_address_null.py
+++ src/core/migrations/0100_contact_ip_address_null.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('cms', '0099_alter_accountrole_options'),
+        ('core', '0099_alter_accountrole_options'),
     ]
 
     operations = [
