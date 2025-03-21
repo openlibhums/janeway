6f4b02ccee123aa3b9f1bb726a3a92026f126654
Mauro MSL <mauro.msl@outlook.com>
2025-03-21 18:23:01 +0000

    feat: Adds resolver for stateless site URLs #4667

--- src/core/templatetags/fqdn.py
+++ src/core/templatetags/fqdn.py
@@ -44,6 +44,27 @@ def site_url(context, url_name=None, *args):
         return path
 
 
+@register.simple_tag
+def stateless_site_url(site, url_name=None, query=None, *args):
+    """ A tag for constructing a url for a site without global request state
+    This should eventually become the canonical site builder for all other
+    tags in this file
+    :param site: The site model instance for which to construct the url
+    :type site: core.model_utils.AbstractSiteModel
+    :param url_name: A django url name to match
+    :type url_name: str
+    :param query: An encoded url querystring
+    :type query: str
+    :param *args: additional arguments for reversing the url by name
+    """
+    if url_name is not None:
+        path = reverse(url_name, args=args)
+    else:
+        path = None
+
+    return site.site_url(query=query, path=path)
+
+
 @register.simple_tag(takes_context=True)
 def journal_base_url(context, journal):
     return journal.site_url()
