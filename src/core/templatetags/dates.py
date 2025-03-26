c7226ba1d639b8514a84ccaefa8758e0283264e5
Andy Byers <a.byers@bbk.ac.uk>
2025-03-26 10:54:26 +0000

    fix: updates offset_date tag to use user's preferred timezone (#4666)

--- src/core/templatetags/dates.py
+++ src/core/templatetags/dates.py
@@ -1,16 +1,21 @@
+from datetime import timedelta
+
+import pytz
 from django import template
 from django.utils import timezone
 
 
+
 register = template.Library()
 
-@register.simple_tag
-def offset_date(days=0, input_type="date"):
+@register.simple_tag(takes_context=True)
+def offset_date(context, days=0, input_type="date"):
     """
     Get a string representing today's date or the now datetime,
     with an offset of X days.
+    :param context: template context, needed to access request.user
     :param days: number of days from now that the date should be set
-    :param date: date or datetime-local
+    :param input_type: 'date' or 'datetime-local'
 
     Usage:
 
@@ -23,7 +28,21 @@ def offset_date(days=0, input_type="date"):
     See admin.core.widgets.soon_date_buttons for a use case.
 
     """
-    due = timezone.now() + timezone.timedelta(days=days)
+    request = context.get("request", None)
+    now = timezone.now()
+
+    if request and hasattr(request, "user"):
+        tz_name = request.user.preferred_timezone
+
+        if tz_name:
+            try:
+                tz = pytz.timezone(tz_name)
+                now = now.astimezone(tz)
+            except pytz.UnknownTimeZoneError:
+                pass
+
+    due = now + timedelta(days=days)
+
     if input_type == "date":
         return due.strftime("%Y-%m-%d")
     elif input_type == "datetime-local":
