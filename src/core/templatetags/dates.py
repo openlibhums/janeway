db800e84614867d8a1f8cca3963bd441db2b0c71
StephDriver <5330770+StephDriver@users.noreply.github.com>
2025-04-01 12:59:35 +0100

    a11y: #4210 make human readable dates translatable

--- src/core/templatetags/dates.py
+++ src/core/templatetags/dates.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 import pytz
 from django import template
 from django.utils import timezone
+from django.utils.translation import gettext as _
 from datetime import datetime
 
 
@@ -53,5 +54,6 @@ def offset_date(context, days=0, input_type="date"):
 def date_human(value):
     """Convert a date to a human readable Day Month(text) Year format e.g. 3rd January 2025"""
     if isinstance(value, datetime):
-        return value.strftime('%d %B %Y')  # e.g., "14 March 2024"
+        # Translators: this creates a human readable date e.g., "14 March 2024"
+        return _('%(day)s %(month)s %(year)s.') % {'day': value.strftime('%d'), 'month': value.strftime('%B'), 'year':value.strftime('%Y'), }
     return value
