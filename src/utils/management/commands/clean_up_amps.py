730fcc52dec1641cb326027991b01594ed2b3cd5
Mauro MSL <mauro.msl@outlook.com>
2025-04-23 10:37:36 +0100

    feat: handle edge-cases gracefully on command

--- src/utils/management/commands/clean_up_amps.py
+++ src/utils/management/commands/clean_up_amps.py
@@ -1,8 +1,11 @@
-from submission.models import Article
 from re import sub
+import sys
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
 from tqdm import tqdm
 
-from django.core.management.base import BaseCommand
+from submission.models import Article
 
 
 class Command(BaseCommand):
@@ -17,6 +20,15 @@ class Command(BaseCommand):
     help = "Removes commented markup and long strings of ampersands from article abstracts."
 
     def handle(self, *args, **options):
+        if not settings.BLEACH_STRIP_COMMENTS:
+            self.stdout.write(self.style.ERROR(
+                "BLEACH_STRIP_COMMENTS setting is not enabled"
+            ))
+
         articles = Article.active_objects.filter(abstract__contains="&amp;amp;")
         for article in tqdm(articles):
             article.save()
+            if article.abstract and "&amp;amp;" in article.abstract:
+                tqdm.write(self.style.ERROR(
+                    f"Failed to clean article: {article}"
+                ))
