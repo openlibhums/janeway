b06a4d3f43be09c27fc759624704992e0c1506d8
Mauro MSL <mauro.msl@outlook.com>
2025-06-05 10:29:03 +0100

    chore: bump version v1.7.5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Andy Byers, Mauro Sanchez & Martin Paul Eve'
 author = 'Andy Byers, Mauro Sanchez & Martin Paul Eve'
 
 # The short X.Y version
-version = '1.7.4'
+version = '1.7.5'
 # The full version, including alpha/beta/rc tags
-release = '1.7.4'
+release = '1.7.5'
 
 # -- General configuration ---------------------------------------------------
 
