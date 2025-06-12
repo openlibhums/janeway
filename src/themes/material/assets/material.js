6df163b05f0896e9a6bd0334905efc9b7a4da4a2
StephDriver <5330770+StephDriver@users.noreply.github.com>
2025-06-12 12:27:58 +0100

    a11y: #4221 review and update links across reader interface

--- src/themes/material/assets/material.js
+++ src/themes/material/assets/material.js
@@ -1,15 +1,15 @@
 function figure_downloads() {
-	var figs = $("div[id^=F]");
+	var figs = $("div[id^=f]");
 	$( figs ).each( function( index, element ){
-	    $(this).find('.fig-caption').prepend('<p class="fig-download"><i class="fa fa-download">&nbsp;</i><a target="_blank" href="' + $( this ).find('img').attr('src') +'">Download</a></p>' );
-	});
+        var divId = $(this).attr('id');
+	    $(this).find('.fig-caption').prepend('<p class="fig-download"><i class="fa fa-download">&nbsp;</i><a target="_blank" aria-describedby="' + divId + '" href="' + $( this ).find('img').attr('src') +'">Download</a></p>' );	});
 }
 
 function table_downloads() {
 	var tables = $("div[id^=T]");
 	$( tables ).each( function( index, element ){
-	    $(this).find('.table-caption').prepend('<p class="fig-download"><i class="fa fa-download">&nbsp;</i><a target="_blank" href="table/' + $( this ).attr('id') +'">Download</a></p>' );
-	});
+        var divId = $(this).attr('id');
+	    $(this).find('.table-caption').prepend('<p class="fig-download"><i class="fa fa-download">&nbsp;</i><a target="_blank" aria-describedby="' + divId + '" href="table/' + $( this ).attr('id') +'">Download</a></p>' );	});
 }
 
 
