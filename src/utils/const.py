8ca6e4dd08e9ea076958c3392de4f3e0dea75a34
Andy Byers <a.byers@bbk.ac.uk>
2025-06-18 13:41:27 +0100

    chore: remove now unused functions.

--- src/utils/const.py
+++ src/utils/const.py
@@ -28,11 +28,3 @@ def get_allowed_attributes_minimal():
 
 def get_allowed_css_styles():
     return _all_styles
-
-def get_jats_allowed_tags():
-    return ["italic", "bold", "sup", "sub", "underline", "xref"]
-
-def get_jats_allowed_attrs():
-    return {
-        "xref": ["ref-type", "rid"],
-    }
