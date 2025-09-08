825b61224c696483b0e28bcbc01e7da831cef35e
StephDriver <5330770+StephDriver@users.noreply.github.com>
2025-09-08 11:48:35 +0100

    a11y: #4694 a11y-info link footers to a11y pages

--- src/journal/views.py
+++ src/journal/views.py
@@ -2125,6 +2125,24 @@ def contact(request):
 
 
 @decorators.frontend_enabled
+def accessibility(request):
+    """
+    Displays the accessibility information page.
+    :param request: HttpRequest object
+    :return: HttpResponse object
+    """
+    if request.journal and request.journal.disable_front_end:
+        template = "admin/core/a11y.html"
+    elif request.journal:
+        template = "core/a11y.html"
+    else:
+        template = "press/a11y.html"
+
+    context = {}
+    return render(request, template, context)
+
+
+@decorators.frontend_enabled
 def editorial_team(request, group_id=None):
     """
     Displays a list of editorial team members at the journal level,
