a18d61364809f938911a1580c5a31a0dfe056e1d
Siobhan <148049561+S-Haime@users.noreply.github.com>
2025-09-17 11:54:22 +0100

    Update 0048_submissionconfiguration_submission_file_text.py
    Signed-off-by: Siobhan <148049561+S-Haime@users.noreply.github.com>

--- src/submission/migrations/0048_submissionconfiguration_submission_file_text.py
+++ src/submission/migrations/0048_submissionconfiguration_submission_file_text.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
             name="submission_file_text",
             field=models.CharField(
                 default="Manuscript File",
-                help_text="During submission, the author will be asked to upload a filethat is considered the main text of the article. You can usethis field to change the label for that file in submission.",
+                help_text="During submission the author will be asked to upload a filethat is considered the main text of the article. You can usethis field to change the label for that file in submission.",
                 max_length=255,
             ),
         ),
