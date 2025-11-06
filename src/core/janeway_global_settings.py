7ce13598e4446ddffae51b9ba2caa80fb96a59cb
Andy Byers <a.byers@bbk.ac.uk>
2025-11-06 11:29:18 +0000

    formatting: adds ruff formatting

--- src/core/janeway_global_settings.py
+++ src/core/janeway_global_settings.py
@@ -584,8 +584,8 @@ OIDC_OP_JWKS_ENDPOINT = os.environ.get("OIDC_OP_JWKS_ENDPOINT")
 
 if ENABLE_OIDC:
     AUTHENTICATION_BACKENDS = (
-        'django.contrib.auth.backends.ModelBackend',
-        'utils.oidc.JanewayOIDCAB',
+        "django.contrib.auth.backends.ModelBackend",
+        "utils.oidc.JanewayOIDCAB",
     )
 
 CORE_FILETEXT_MODEL = "core.FileText"
