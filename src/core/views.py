10cc0fcd88cf027cf53aae2d1996a737656d5340
Esther Verreau <esther.verreau@ucop.edu>
2026-02-13 14:14:51 -0600

    add date orcid request was sent

--- src/core/views.py
+++ src/core/views.py
@@ -1577,7 +1577,7 @@ def user_edit(request, user_id):
             messages.add_message(
                 request,
                 messages.SUCCESS,
-                _("Successfully requested ORCiD from user."),
+                _("Successfully requested ORCID iD from user."),
             )
         else:
             form = forms.EditAccountForm(request.POST, request.FILES, instance=user)
