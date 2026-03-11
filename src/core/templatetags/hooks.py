bb8b2cebfb1761f119cab3d4943c6251dbeae70a
Andy Byers <a.byers@bbk.ac.uk>
2026-03-11 15:38:12 +0000

    refactor: simplify return logic in hooks

--- src/core/templatetags/hooks.py
+++ src/core/templatetags/hooks.py
@@ -12,19 +12,16 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def hook(context, hook_name, default_value='', *args, **kwargs):
-    try:
-        html = ""
-        for hook in settings.PLUGIN_HOOKS.get(hook_name, []):
+    html = ""
+    for hook in settings.PLUGIN_HOOKS.get(hook_name, []):
+        try:
             hook_module = import_module(hook.get("module"))
             function = getattr(hook_module, hook.get("function"))
             hook_output = function(context, *args, **kwargs)
-
             if hook_output:
-                html = html + hook_output
-        if html:
-            return mark_safe(html)
-    except Exception as e:
-        logger.error("Error rendering hook {0}: {1}".format(hook_name, e))
-        if settings.DEBUG:
-            return f"[DEBUG] Error rendering hook output: {e}"
-    return mark_safe(default_value)
+                html += hook_output
+        except Exception as e:
+            logger.error("Error rendering hook {0}: {1}".format(hook_name, e))
+            if settings.DEBUG:
+                return f"[DEBUG] Error rendering hook output: {e}"
+    return mark_safe(html or default_value)
