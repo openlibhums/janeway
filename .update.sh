3879f8f9ee030384b8faef1c0f11994639c8f3c0
Andy Byers <a.byers@bbk.ac.uk>
2026-09-17 14:05:56 +0100

    chore: remove duplicate collectstatic, this is covered by build_assets

--- .update.sh
+++ .update.sh
@@ -6,7 +6,6 @@ set -euo pipefail
 pip3 install -r requirements.txt
 python3 src/manage.py migrate
 python3 src/manage.py build_assets
-python3 src/manage.py collectstatic --no-input
 DJANGO_SETTINGS_MODULE=1 python3 src/manage.py compilemessages
 python3 src/manage.py load_default_settings
 python3 src/manage.py update_repository_settings
