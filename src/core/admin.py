f2ca74dc0252e81ae815e5c0c64385b84b5f0436
Esther Verreau <esther.verreau@ucop.edu>
2026-09-22 15:29:42 -0500

    ruff

--- src/core/admin.py
+++ src/core/admin.py
@@ -702,33 +702,40 @@ class OrganizationAdmin(admin.ModelAdmin):
         )
 
         if search_term:
-            queryset = queryset.annotate(
-                relevance=Case(
-                    When(ror_id__iexact=search_term, then=Value(100)),
-                    When(acronyms__value__iexact=search_term, then=Value(100)),
-                    When(aliases__value__iexact=search_term, then=Value(90)),
-                    When(custom_label__value__iexact=search_term, then=Value(90)),
-                    When(ror_display__value__iexact=search_term, then=Value(90)),
-
-                    When(ror_id__istartswith=search_term, then=Value(80)),
-                    When(custom_label__value__istartswith=search_term, then=Value(70)),
-                    When(ror_display__value__istartswith=search_term, then=Value(70)),
-
-                    When(ror_id__icontains=search_term, then=Value(60)),
-                    When(custom_label__value__icontains=search_term, then=Value(50)),
-                    When(ror_display__value__icontains=search_term, then=Value(50)),
-
-                    When(labels__value__icontains=search_term, then=Value(40)),
-                    When(aliases__value__icontains=search_term, then=Value(40)),
-                    When(acronyms__value__icontains=search_term, then=Value(40)),
-                    When(website__icontains=search_term, then=Value(20)),
-
-                    default=Value(0),
-                    output_field=IntegerField(),
+            queryset = (
+                queryset.annotate(
+                    relevance=Case(
+                        When(ror_id__iexact=search_term, then=Value(100)),
+                        When(acronyms__value__iexact=search_term, then=Value(100)),
+                        When(aliases__value__iexact=search_term, then=Value(90)),
+                        When(custom_label__value__iexact=search_term, then=Value(90)),
+                        When(ror_display__value__iexact=search_term, then=Value(90)),
+                        When(ror_id__istartswith=search_term, then=Value(80)),
+                        When(
+                            custom_label__value__istartswith=search_term, then=Value(70)
+                        ),
+                        When(
+                            ror_display__value__istartswith=search_term, then=Value(70)
+                        ),
+                        When(ror_id__icontains=search_term, then=Value(60)),
+                        When(
+                            custom_label__value__icontains=search_term, then=Value(50)
+                        ),
+                        When(ror_display__value__icontains=search_term, then=Value(50)),
+                        When(labels__value__icontains=search_term, then=Value(40)),
+                        When(aliases__value__icontains=search_term, then=Value(40)),
+                        When(acronyms__value__icontains=search_term, then=Value(40)),
+                        When(website__icontains=search_term, then=Value(20)),
+                        default=Value(0),
+                        output_field=IntegerField(),
+                    )
                 )
-            ).order_by("-relevance").distinct()
+                .order_by("-relevance")
+                .distinct()
+            )
 
         return queryset, may_have_duplicates
+
 
 class OrganizationNameAdmin(admin.ModelAdmin):
     list_display = ("pk", "value", "language")
