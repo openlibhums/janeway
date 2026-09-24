d7bf6a8a0e0d9bc9ca0b316184f9c51b385159e0
Andy Byers <a.byers@bbk.ac.uk>
2026-09-24 12:01:29 +0100

    fix: label the clean current issue link with the current issue

--- src/utils/tests/test_wcag_audit_fixes.py
+++ src/utils/tests/test_wcag_audit_fixes.py
@@ -11,6 +11,33 @@ from utils.testing import helpers
 
 
 @override_settings(URL_CONFIG="domain")
+class IssueLinkLabelTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.press = helpers.create_press()
+        cls.journal_one, cls.journal_two = helpers.create_journals()
+        cls.issue_one = helpers.create_issue(cls.journal_one, vol=1, number=1)
+        cls.issue_two = helpers.create_issue(cls.journal_one, vol=1, number=2)
+        cls.journal_one.current_issue = cls.issue_two
+        cls.journal_one.save()
+
+    def setUp(self):
+        clear_cache()
+
+    def test_clean_current_issue_link_is_labelled_with_current_issue(self):
+        response = self.client.get(
+            reverse("journal_issues"),
+            {"theme": "clean"},
+            SERVER_NAME=self.journal_one.domain,
+        )
+        self.assertContains(
+            response,
+            f"aria-label='{self.issue_two.display_title_a11y} (0 items)'",
+        )
+        self.assertNotContains(response, "aria-label=' (")
+
+
+@override_settings(URL_CONFIG="domain")
 class JournalLogoFallbackTests(TestCase):
     @classmethod
     def setUpTestData(cls):
